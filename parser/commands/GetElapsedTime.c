9aa394d4095f17bd14d466f43db5af083168c533
dennis <dennis.podkovyrin@gmail.com>
2020-08-27 00:28:21 +0700

    time_t size issue workaround for :get-elapsed-time

--- parser/commands/GetElapsedTime.c
+++ parser/commands/GetElapsedTime.c
@@ -1,6 +1,6 @@
-#include <time.h>
 #include <libxpl/xplcore.h>
 #include "commands/GetElapsedTime.h"
+#include <time.h>
 
 void xplCmdProfileCheckpointPrologue(xplCommandInfoPtr commandInfo)
 {
@@ -14,7 +14,13 @@ void xplCmdGetElapsedTimeEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr re
 
 	time(&current);
 	old = commandInfo->document->main->profile_checkpoint;
-	sprintf((char*) buf, "%lu", current - old);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat"
+	if (sizeof(current) == 4)
+		sprintf((char*) buf, "%lu", current - old);
+	else
+		sprintf((char*) buf, "%I64u", current - old);
+#pragma GCC diagnostic pop
 	ret = xmlNewDocText(commandInfo->document->document, buf);
 	ASSIGN_RESULT(ret, false, true);
 	commandInfo->document->main->profile_checkpoint = current;
