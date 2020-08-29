97312392db3cd9d4fce4366391127e2889861376
dennis <dennis.podkovyrin@gmail.com>
2020-08-30 05:10:54 +0700

    reworked :choose

--- parser/commands/Choose.c
+++ parser/commands/Choose.c
@@ -1,22 +1,42 @@
 #include <libxpl/xpltree.h>
 #include "commands/Choose.h"
 
+typedef struct _xplCmdChooseParams
+{
+	bool repeat;
+} xplCmdChooseParams, *xplCmdChooseParamsPtr;
+
+static const xplCmdChooseParams params_stencil =
+{
+	.repeat = false
+};
+
+xplCommand xplChooseCommand =
+{
+	.prologue = xplCmdChoosePrologue,
+	.epilogue = xplCmdChooseEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdChooseParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdChoosePrologue(xplCommandInfoPtr commandInfo)
 {
+	UNUSED_PARAM(commandInfo);
 }
 
 void xplCmdChooseEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define REPEAT_ATTR (BAD_CAST "repeat")
-	bool repeat;
-	xmlNodePtr error;
+	xplCmdChooseParamsPtr params = (xplCmdChooseParamsPtr) commandInfo->params;
 
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, REPEAT_ATTR, &repeat, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	ASSIGN_RESULT(xplDetachContent(commandInfo->element), repeat, true);
+	ASSIGN_RESULT(xplDetachContent(commandInfo->element), params->repeat, true);
 }
-
-xplCommand xplChooseCommand = { xplCmdChoosePrologue, xplCmdChooseEpilogue };
