c0aba75f41d1af4a9c59338e80da27e590c007eb
dennis <dennis.podkovyrin@gmail.com>
2020-08-30 06:03:40 +0700

    reworked :crash

--- parser/commands/Crash.c
+++ parser/commands/Crash.c
@@ -1,14 +1,24 @@
 #include "commands/Crash.h"
 
+xplCommand xplCrashCommand =
+{
+	.prologue = xplCmdCrashPrologue,
+	.epilogue = xplCmdCrashEpilogue,
+	.flags = 0,
+	.params_stencil = NULL
+};
+
 void xplCmdCrashPrologue(xplCommandInfoPtr commandInfo)
 {
+	UNUSED_PARAM(commandInfo);
 }
 
 void xplCmdCrashEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
 	int divisor = 0;
 	int dividend = 42;
+
+	UNUSED_PARAM(commandInfo);
+	UNUSED_PARAM(result);
 	*((int*) (size_t) (dividend / divisor)) = dividend / divisor;
 }
-
-xplCommand xplCrashCommand = { xplCmdCrashPrologue, xplCmdCrashEpilogue };
