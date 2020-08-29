960876bca74964eb5488f864e0b02f9433adf58f
dennis <dennis.podkovyrin@gmail.com>
2020-08-30 05:37:00 +0700

    reworked :container

--- parser/commands/Container.c
+++ parser/commands/Container.c
@@ -2,37 +2,54 @@
 #include <libxpl/xpltree.h>
 #include "commands/Container.h"
 
+typedef struct _xplCmdContainerParams
+{
+	bool repeat;
+	bool return_content;
+} xplCmdContainerParams, *xplCmdContainerParamsPtr;
+
+static const xplCmdContainerParams params_stencil =
+{
+	.repeat = false,
+	.return_content = true
+};
+
+xplCommand xplContainerCommand =
+{
+	.prologue = xplCmdContainerPrologue,
+	.epilogue = xplCmdContainerEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdContainerParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = BAD_CAST "returncontent",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.return_content
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdContainerPrologue(xplCommandInfoPtr commandInfo)
 {
+	UNUSED_PARAM(commandInfo);
 }
 
 void xplCmdContainerEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define REPEAT_ATTR BAD_CAST "repeat"
-#define RETURNCONTENT_ATTR BAD_CAST "returncontent"
+	xplCmdContainerParamsPtr params = (xplCmdContainerParamsPtr) commandInfo->params;
 
-	bool repeat;
-	bool return_content;
-	xmlNodePtr error;
-
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, REPEAT_ATTR, &repeat, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, RETURNCONTENT_ATTR, &return_content, true)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	if (return_content)
-	{
-		ASSIGN_RESULT(xplDetachContent(commandInfo->element), repeat, true);
-	} else {
+	if (params->return_content)
+		ASSIGN_RESULT(xplDetachContent(commandInfo->element), params->repeat, true);
+	else {
 		if (commandInfo->element->children)
 			xplDocDeferNodeListDeletion(commandInfo->document, xplDetachContent(commandInfo->element->children));
 		ASSIGN_RESULT(NULL, false, true);
 	}
 }
-
-xplCommand xplContainerCommand = { xplCmdContainerPrologue, xplCmdContainerEpilogue };
