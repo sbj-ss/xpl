c5a32b095f19a2d6eea5a7f6feea7b680fd9e1b5
dennis <dennis.podkovyrin@gmail.com>
2020-08-30 19:31:08 +0700

    reworked :error

--- parser/commands/Error.c
+++ parser/commands/Error.c
@@ -1,8 +1,17 @@
 #include <libxpl/xplmessages.h>
 #include "commands/Error.h"
 
+xplCommand xplErrorCommand =
+{
+	.prologue = xplCmdErrorPrologue,
+	.epilogue = xplCmdErrorEpilogue,
+	.flags = 0,
+	.params_stencil = NULL
+};
+
 void xplCmdErrorPrologue(xplCommandInfoPtr commandInfo)
 {
+	UNUSED_PARAM(commandInfo);
 }
 
 void xplCmdErrorEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
@@ -13,5 +22,3 @@ void xplCmdErrorEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 	ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "%s", txt), true, true);
 	if (txt) XPL_FREE(txt);
 }
-
-xplCommand xplErrorCommand = { xplCmdErrorPrologue, xplCmdErrorEpilogue };
