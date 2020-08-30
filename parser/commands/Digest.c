f13e4410ec07a638dc56f379f153bc3669197c02
dennis <dennis.podkovyrin@gmail.com>
2020-08-30 08:24:45 +0700

    reworked :digest

--- parser/commands/Digest.c
+++ parser/commands/Digest.c
@@ -9,72 +9,92 @@
 #include "openssl/sha.h"
 #include "openssl/whrlpool.h"
 
+typedef unsigned char* (*xplCmdDigestFunc)(const unsigned char *d, size_t n, unsigned char *md);
+
+typedef struct _xplCmdDigestMethodDesc
+{
+	xmlChar *name;
+	xplCmdDigestFunc fn;
+	size_t size;
+} xplCmdDigestMethodDesc, *xplCmdDigestMethodDescPtr;
+
+static xplCmdDigestMethodDesc digest_methods[] =
+{
+	{ BAD_CAST "md4", MD4, MD4_DIGEST_LENGTH },
+	{ BAD_CAST "md5", MD5, MD5_DIGEST_LENGTH },
+	{ BAD_CAST "ripemd-160", RIPEMD160, RIPEMD160_DIGEST_LENGTH },
+	{ BAD_CAST "sha1", SHA1, SHA_DIGEST_LENGTH },
+	{ BAD_CAST "sha-224", SHA224, SHA224_DIGEST_LENGTH },
+	{ BAD_CAST "sha-256", SHA256, SHA256_DIGEST_LENGTH },
+	{ BAD_CAST "sha-384", SHA384, SHA384_DIGEST_LENGTH },
+	{ BAD_CAST "sha-512", SHA512, SHA512_DIGEST_LENGTH },
+	{ BAD_CAST "whirlpool", (xplCmdDigestFunc) WHIRLPOOL, WHIRLPOOL_DIGEST_LENGTH },
+	{ NULL, NULL, 0 }
+};
+
+typedef struct _xplCmdDigestParams
+{
+	xplCmdDigestMethodDescPtr method;
+} xplCmdDigestParams, *xplCmdDigestParamsPtr;
+
+static const xplCmdDigestParams params_stencil =
+{
+	.method = NULL
+};
+
+static xmlChar* _getMethod(const xmlChar* raw_value, void **result)
+{
+	xplCmdDigestMethodDescPtr desc = digest_methods;
+
+	while (desc->name)
+	{
+		if (!xmlStrcasecmp(desc->name, raw_value))
+		{
+			*result = desc;
+			return NULL;
+		}
+		desc++;
+	}
+	return xplFormatMessage(BAD_CAST "unknown digest method '%s'", raw_value);
+}
+
+xplCommand xplDigestCommand =
+{
+	.prologue = xplCmdDigestPrologue,
+	.epilogue = xplCmdDigestEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE | XPL_CMD_FLAG_CONTENT_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdDigestParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "method",
+			.type = XPL_CMD_PARAM_TYPE_PTR_CUSTOM_GETTER,
+			.extra = {
+				.ptr_fn = {
+					.getter = _getMethod,
+					.deallocator = NULL
+				}
+			},
+			.value_stencil = &params_stencil.method
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdDigestPrologue(xplCommandInfoPtr commandInfo)
 {
+	UNUSED_PARAM(commandInfo);
 }
 
 void xplCmdDigestEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define METHOD_ATTR (BAD_CAST "method")
-
-	xmlChar *method_attr = NULL;
-	xmlChar *content = NULL;
+	xplCmdDigestParamsPtr params = (xplCmdDigestParamsPtr) commandInfo->params;
 	xmlNodePtr ret;
 	unsigned char *digest;
-	size_t digest_size;
 
-	method_attr = xmlGetNoNsProp(commandInfo->element, METHOD_ATTR);
-	if (!method_attr)
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "no digest method specified"), true, true);
-		goto done;
-	}
-	if (!xplCheckNodeListForText(commandInfo->element->children))
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "non-text nodes inside"), true, true);
-		goto done;
-	}
-	content = xmlNodeListGetString(commandInfo->element->doc, commandInfo->element->children, 1);
-	if (!xmlStrcasecmp(method_attr, BAD_CAST "md4"))
-	{
-		digest = MD4(content, xmlStrlen(content), NULL);
-		digest_size = MD4_DIGEST_LENGTH;
-	} else if (!xmlStrcasecmp(method_attr, BAD_CAST "md5")) {
-		digest = MD5(content, xmlStrlen(content), NULL);
-		digest_size = MD5_DIGEST_LENGTH;
-	} else if (!xmlStrcasecmp(method_attr, BAD_CAST "ripemd-160")) {
-		digest = RIPEMD160(content, xmlStrlen(content), NULL);
-		digest_size = RIPEMD160_DIGEST_LENGTH;
-	} else if (!xmlStrcasecmp(method_attr, BAD_CAST "sha1")) {
-		digest = SHA1(content, xmlStrlen(content), NULL);
-		digest_size = SHA_DIGEST_LENGTH;
-	} else if (!xmlStrcasecmp(method_attr, BAD_CAST "sha-224")) {
-		digest = SHA224(content, xmlStrlen(content), NULL);
-		digest_size = SHA224_DIGEST_LENGTH;
-	} else if (!xmlStrcasecmp(method_attr, BAD_CAST "sha-256")) {
-		digest = SHA256(content, xmlStrlen(content), NULL);
-		digest_size = SHA256_DIGEST_LENGTH;
-	} else if (!xmlStrcasecmp(method_attr, BAD_CAST "sha-384")) {
-		digest = SHA384(content, xmlStrlen(content), NULL);
-		digest_size = SHA384_DIGEST_LENGTH;
-	} else if (!xmlStrcasecmp(method_attr, BAD_CAST "sha-512")) {
-		digest = SHA512(content, xmlStrlen(content), NULL);
-		digest_size = SHA512_DIGEST_LENGTH;
-	} else if (!xmlStrcasecmp(method_attr, BAD_CAST "whirlpool")) {
-		digest = WHIRLPOOL(content, xmlStrlen(content), NULL);
-		digest_size = WHIRLPOOL_DIGEST_LENGTH;
-	} else {
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "unknown method: %s", method_attr), true, true);
-		goto done;
-	}
+	digest = params->method->fn(commandInfo->content, xmlStrlen(commandInfo->content), NULL);
 	ret = xmlNewDocText(commandInfo->document->document, NULL);
-	ret->content = xstrBufferToHex(digest, digest_size, false);
+	ret->content = xstrBufferToHex(digest, params->method->size, false);
 	ASSIGN_RESULT(ret, false, true);
-done:
-	if (method_attr)
-		XPL_FREE(method_attr);
-	if (content)
-		XPL_FREE(content);
 }
-
-xplCommand xplDigestCommand = { xplCmdDigestPrologue, xplCmdDigestEpilogue };
