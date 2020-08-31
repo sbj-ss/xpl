1ba2ff8cd48f11044bdd662f5cbe0ef5d6171645
dennis <dennis.podkovyrin@gmail.com>
2020-09-01 03:47:36 +0700

    reworked :is-defined

--- parser/commands/IsDefined.c
+++ parser/commands/IsDefined.c
@@ -3,70 +3,78 @@
 #include <libxpl/xpltree.h>
 #include "commands/IsDefined.h"
 
+typedef struct _xplCmdIsDefinedParams
+{
+	xplQName name;
+	xmlXPathObjectPtr at;
+} xplCmdIsDefinedParams, *xplCmdIsDefinedParamsPtr;
+
+static const xplCmdIsDefinedParams params_stencil =
+{
+	.name = { NULL, NULL },
+	.at = NULL
+};
+
+xplCommand xplIsDefinedCommand =
+{
+	.prologue = xplCmdIsDefinedPrologue,
+	.epilogue = xplCmdIsDefinedEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdIsDefinedParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "name",
+			.type = XPL_CMD_PARAM_TYPE_QNAME,
+			.required = true,
+			.value_stencil = &params_stencil.name
+		}, {
+			.name = BAD_CAST "at",
+			.type = XPL_CMD_PARAM_TYPE_XPATH,
+			.extra.xpath_type = XPL_CMD_PARAM_XPATH_TYPE_NODESET,
+			.value_stencil = &params_stencil.at
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdIsDefinedPrologue(xplCommandInfoPtr commandInfo)
 {
+	UNUSED_PARAM(commandInfo);
 }
 
+// TODO warning if at evaluates into multiple nodes
+// TODO error if at evaluates into non-elements
+// TODO do we check right at selected elements?
 void xplCmdIsDefinedEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define NAME_ATTR (BAD_CAST "name")
-#define AT_ATTR (BAD_CAST "at")
-	xmlChar *name_attr = NULL, *tagname;
-	xmlChar *at_attr = NULL;
-	xmlNsPtr ns;
-	xmlXPathObjectPtr sel = NULL;
+	xplCmdIsDefinedParamsPtr params = (xplCmdIsDefinedParamsPtr) commandInfo->params;
 	xmlHashTablePtr macro_table;
 	xplMacroPtr macro = NULL;
 	const xmlChar* value;
 	xmlNodePtr ret;
 	size_t i;
 
-	name_attr = xmlGetNoNsProp(commandInfo->element, NAME_ATTR);
-	if (!name_attr)
+	if (params->at)
 	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "missing name attribute"), true, true);
-		return;
-	}
-	EXTRACT_NS_AND_TAGNAME(name_attr, ns, tagname, commandInfo->element);
-	at_attr = xmlGetNoNsProp(commandInfo->element, AT_ATTR);
-	if (at_attr)
-	{
-		sel = xplSelectNodes(commandInfo, commandInfo->element, at_attr);
-		if (sel)
+		if (params->at->nodesetval)
 		{
-			if (sel->type == XPATH_NODESET)
+			for (i = 0; i < (size_t) params->at->nodesetval->nodeNr; i++)
 			{
-				if (sel->nodesetval)
-				{
-					for (i = 0; i < (size_t) sel->nodesetval->nodeNr; i++)
-					{
-						macro_table = (xmlHashTablePtr) sel->nodesetval->nodeTab[i]->_private;
-						if (macro_table)
-							macro = (xplMacroPtr) xmlHashLookup2(macro_table, tagname, ns? ns->href: NULL);
-						if (macro && !macro->disabled_spin) 
-							break;
-					}
-				}
-			} else {
-				ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "at XPath expression \"%s\" evaluated to non-nodeset value", at_attr), true, true);
-				goto done;
+				macro_table = (xmlHashTablePtr) params->at->nodesetval->nodeTab[i]->_private;
+				if (macro_table)
+					macro = (xplMacroPtr) xmlHashLookup2(macro_table, params->name.ncname, params->name.ns? params->name.ns->href: NULL);
+				if (macro && !macro->disabled_spin)
+					break;
 			}
-		} else {
-			ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "invalid at XPath expression \"%s\"", at_attr), true, true);
-			goto done;
 		}
 	} else
-		macro = xplMacroLookup(commandInfo->element->parent, ns? ns->href: NULL, tagname);
+		macro = xplMacroLookup(commandInfo->element->parent, params->name.ns? params->name.ns->href: NULL, params->name.ncname);
 	if (macro && !macro->disabled_spin)
 		value = BAD_CAST "true";
 	else
 		value = BAD_CAST "false";
 	ret = xmlNewDocText(commandInfo->document->document, value);
 	ASSIGN_RESULT(ret, false, true);
-done:
-	if (name_attr) XPL_FREE(name_attr);
-	if (at_attr) XPL_FREE(at_attr);
-	if (sel) xmlXPathFreeObject(sel);
 }
-
-xplCommand xplIsDefinedCommand = { xplCmdIsDefinedPrologue, xplCmdIsDefinedEpilogue }; 
