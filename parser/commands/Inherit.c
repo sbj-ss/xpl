053f4afa7280154d7046c16a60794c534dd8d4cd
dennis <dennis.podkovyrin@gmail.com>
2020-09-01 02:39:18 +0700

    reworked :inherit

--- parser/commands/Inherit.c
+++ parser/commands/Inherit.c
@@ -4,53 +4,95 @@
 #include <libxpl/xpltree.h>
 #include "commands/Inherit.h"
 
+typedef struct _xplCmdInheritParams
+{
+	xplQName name;
+	bool skip_current;
+	bool repeat;
+} xplCmdInheritParams, *xplCmdInheritParamsPtr;
+
+static const xplCmdInheritParams params_stencil =
+{
+	.name = { NULL, NULL },
+	.skip_current = true,
+	.repeat = false
+};
+
+xplCommand xplInheritCommand =
+{
+	.prologue = xplCmdInheritPrologue,
+	.epilogue = xplCmdInheritEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdInheritParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "name",
+			.type = XPL_CMD_PARAM_TYPE_QNAME,
+			.value_stencil = &params_stencil.name
+		}, {
+			.name = BAD_CAST "skipcurrent",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.skip_current
+		}, {
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = NULL
+		}
+	}
+};
+
+inline xplMacroPtr _macroLookup(xmlNodePtr node, xplQName name)
+{
+	return xplMacroLookup(node, name.ns? name.ns->href: NULL, name.ncname);
+}
+
 void xplCmdInheritPrologue(xplCommandInfoPtr commandInfo)
 {
+	UNUSED_PARAM(commandInfo);
 }
 
 void xplCmdInheritEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define NAME_ATTR (BAD_CAST "name")
-#define SKIP_CURRENT_ATTR (BAD_CAST "skipcurrent")
-	xmlChar *name_attr = NULL, *tagname;
-	xmlNsPtr ns;
+	xplCmdInheritParamsPtr params = (xplCmdInheritParamsPtr) commandInfo->params;
+	xplQName macro_name;
 	xplMacroPtr macro = NULL;
 	bool skip_current = true;
-	xmlNodePtr ret, error, original_content = NULL, caller, temp_children;
+	xmlNodePtr ret, original_content = NULL, caller, temp_children;
 
 	if (!commandInfo->document->current_macro)
 	{
 		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "not inside macro definition or call"), true, true);
 		return;
 	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, SKIP_CURRENT_ATTR, &skip_current, skip_current)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
+	if (params->name.ncname)
+		macro_name = params->name;
+	else {
+		macro_name.ncname = commandInfo->document->current_macro->name;
+		macro_name.ns = commandInfo->document->current_macro->ns;
 	}
-	name_attr = xmlGetNoNsProp(commandInfo->element, NAME_ATTR);
-	if (!name_attr)
-	{
-		tagname = commandInfo->document->current_macro->name;
-		ns = commandInfo->document->current_macro->ns;
-	} else {
-		EXTRACT_NS_AND_TAGNAME(name_attr, ns, tagname, commandInfo->element);
-	}
-	macro = xplMacroLookup(commandInfo->element, ns? ns->href: NULL, tagname);
+	macro = _macroLookup(commandInfo->element, macro_name);
 	if (skip_current && macro)
 	{
 		caller = macro->caller;
 		original_content = macro->node_original_content;
-		macro = xplMacroLookup(macro->parent->parent, ns? ns->href: NULL, tagname);
+		macro = _macroLookup(macro->parent->parent, macro_name);
 	} else
 		caller = original_content = NULL;
-	if (!macro && cfgWarnOnMissingInheritBase)
-		xplDisplayMessage(xplMsgWarning, BAD_CAST "%s:%s: no macro \"%s:%s\" to inherit from", 
-			commandInfo->element->ns? commandInfo->element->ns->prefix: NULL,
-			commandInfo->element->name,
-			ns? ns->prefix: NULL,
-			tagname);
-	if (macro && macro->content)
+	if (!macro)
+	{
+		if (cfgWarnOnMissingInheritBase)
+			xplDisplayMessage(xplMsgWarning, BAD_CAST "%s:%s: no macro \"%s:%s\" to inherit from",
+				commandInfo->element->ns? commandInfo->element->ns->prefix: NULL,
+				commandInfo->element->name,
+				macro_name.ns? macro_name.ns->prefix: NULL,
+				macro_name.ncname);
+		ASSIGN_RESULT(NULL, false, true);
+		return;
+	}
+	if (macro->content)
 	{
 		if (((macro->expansion_state == XPL_MACRO_EXPAND_ALWAYS) || (macro->expansion_state == XPL_MACRO_EXPAND_ONCE)) && caller)
 		{
@@ -70,8 +112,5 @@ void xplCmdInheritEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 			ret = xplCloneNodeList(macro->content, commandInfo->element->parent, commandInfo->element->doc);
 	} else
 		ret = NULL;
-	ASSIGN_RESULT(ret, true, true);
-	if (name_attr) XPL_FREE(name_attr);
+	ASSIGN_RESULT(ret, params->repeat, true);
 }
-
-xplCommand xplInheritCommand = { xplCmdInheritPrologue, xplCmdInheritEpilogue }; 
