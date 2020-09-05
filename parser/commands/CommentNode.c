eee5436a1dcfdcbe90ca36f2a0d96fcc78824bf9
dennis <dennis.podkovyrin@gmail.com>
2020-09-05 12:39:15 +0700

    simplified :comment-node

--- parser/commands/CommentNode.c
+++ parser/commands/CommentNode.c
@@ -6,7 +6,7 @@ xplCommand xplCommentNodeCommand =
 {
 	.prologue = xplCmdCommentNodePrologue,
 	.epilogue = xplCmdCommentNodeEpilogue,
-	.flags = 0,
+	.flags = XPL_CMD_FLAG_CONTENT_FOR_EPILOGUE,
 	.params_stencil = NULL
 };
 
@@ -17,15 +17,5 @@ void xplCmdCommentNodePrologue(xplCommandInfoPtr commandInfo)
 
 void xplCmdCommentNodeEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-	xmlChar *txt = NULL;
-	xmlNodePtr ret;
-	if (!xplCheckNodeListForText(commandInfo->element->children))
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "non-text nodes inside"), true, true);
-		return;
-	}
-	txt = xmlNodeListGetString(commandInfo->element->doc, commandInfo->element->children, 1);
-	ret = xmlNewDocComment(commandInfo->element->doc, NULL);
-	ret->content = txt;
-	ASSIGN_RESULT(ret, false, true);
+	ASSIGN_RESULT(xmlNewDocComment(commandInfo->element->doc, commandInfo->content), false, true);
 }
