5945e3020a61c2ab3259d70dbf072f8101613b46
dennis <dennis.podkovyrin@gmail.com>
2020-12-11 00:04:09 +0700

    reworked :otherwise

--- parser/commands/Otherwise.c
+++ parser/commands/Otherwise.c
@@ -4,24 +4,43 @@
 
 void xplCmdOtherwiseEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+typedef struct _xplCmdOtherwiseParams
+{
+	bool repeat;
+} xplCmdOtherwiseParams, *xplCmdOtherwiseParamsPtr;
+
+static const xplCmdOtherwiseParams params_stencil =
+{
+	.repeat = false
+};
+
+xplCommand xplOtherwiseCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdOtherwiseEpilogue,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdOtherwiseParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdOtherwiseEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define REPEAT_ATTR (BAD_CAST "repeat")
-	bool repeat;
-	xmlNodePtr brk, error;
+	xplCmdOtherwiseParamsPtr params = (xplCmdOtherwiseParamsPtr) commandInfo->params;
+	xmlNodePtr brk;
 	xmlNsPtr brk_ns;
 
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, REPEAT_ATTR, &repeat, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	ASSIGN_RESULT(xplDetachContent(commandInfo->element), repeat, true);
+	ASSIGN_RESULT(xplDetachContent(commandInfo->element), params->repeat, true);
 	brk_ns = commandInfo->document->root_xpl_ns;
 	if (!brk_ns)
 		brk_ns = xmlSearchNsByHref(commandInfo->element->doc, commandInfo->element, cfgXplNsUri);
 	brk = xmlNewDocNode(commandInfo->element->doc, brk_ns, BAD_CAST "break", NULL);
 	xplAppendList(commandInfo->element, brk);
 }
-
-xplCommand xplOtherwiseCommand = { NULL, xplCmdOtherwiseEpilogue };
