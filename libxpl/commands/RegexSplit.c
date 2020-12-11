a1ce517880b6046b70744cfc0c382031743c9aa5
dennis <dennis.podkovyrin@gmail.com>
2020-12-11 11:03:21 +0700

    reworked :regex-split

--- libxpl/commands/RegexSplit.c
+++ libxpl/commands/RegexSplit.c
@@ -7,6 +7,75 @@
 
 void xplCmdRegexSplitEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+typedef struct _xplCmdRegexSplitParams
+{
+	xmlChar *regex;
+	bool repeat;
+	xplQName tagname;
+	xplQName delimiter_tagname;
+	bool keep_delimiter;
+	bool keep_empty_tags;
+	bool unique;
+} xplCmdRegexSplitParams, *xplCmdRegexSplitParamsPtr;
+
+static const xplCmdRegexSplitParams params_stencil =
+{
+	.regex = NULL,
+	.repeat = true,
+	.tagname = { NULL, NULL },
+	.delimiter_tagname = { NULL, NULL },
+	.keep_delimiter = false,
+	.keep_empty_tags = false,
+	.unique = false
+};
+
+static xmlChar* regex_aliases[] = { BAD_CAST "delimiter", NULL };
+
+xplCommand xplRegexSplitCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdRegexSplitEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE | XPL_CMD_FLAG_CONTENT_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdRegexSplitParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "regex",
+			.aliases = regex_aliases,
+			.type = XPL_CMD_PARAM_TYPE_STRING,
+			.required = true,
+			.value_stencil = &params_stencil.regex
+		}, {
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = BAD_CAST "tagname",
+			.type = XPL_CMD_PARAM_TYPE_QNAME,
+			.required = true,
+			.value_stencil = &params_stencil.tagname
+		}, {
+			.name = BAD_CAST "delimitertagname",
+			.type = XPL_CMD_PARAM_TYPE_QNAME,
+			.value_stencil = &params_stencil.delimiter_tagname
+		}, {
+			.name = BAD_CAST "keepdelimiter",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.keep_delimiter
+		}, {
+			.name = BAD_CAST "keepemptytags",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.keep_empty_tags
+		}, {
+			.name = BAD_CAST "unique",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.unique
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdRegexSplitEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
 #define SELECT_ATTR (BAD_CAST "select")
@@ -30,78 +99,26 @@ void xplCmdRegexSplitEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result
 		}\
 	}
 
-	xmlChar *select_attr = NULL;
-	xmlChar *regex_attr = NULL;
-	xmlChar *tagname_attr = NULL;
-	xmlChar *delimitertagname_attr = NULL;
-	xmlChar *tagname, *delimitertagname;
+	xplCmdRegexSplitParamsPtr params = (xplCmdRegexSplitParamsPtr) commandInfo->params;
 	xmlChar tmp;
-	xmlNsPtr ns, delim_ns;
-	bool keepdelimiter;
-	bool keepemptytags;
-	bool repeat;
-	bool unique;
-	xmlChar *content = NULL, *content_end;
+	xmlChar *content, *content_end;
 	regex_t *regex = NULL;
 	OnigErrorInfo err_info;
 	OnigRegion *region = NULL;
 	xmlChar onig_err_str[ONIG_MAX_ERROR_MESSAGE_LEN+1];
 	int onig_ret_code;
-	xmlNodePtr ret = NULL, tail = NULL, cur, error;
+	xmlNodePtr ret = NULL, tail = NULL, cur;
 	bool match;
 	xmlChar *start, *end, *prev_boundary;
 	xmlHashTablePtr unique_hash = NULL;
-	xmlXPathObjectPtr sel = NULL;
-	size_t i;
 	char marker; 
 
-	regex_attr = xmlGetNoNsProp(commandInfo->element, REGEX_ATTR);
-	if (!regex_attr)
-		regex_attr = xmlGetNoNsProp(commandInfo->element, DELIMITER_ATTR);
-	if (!regex_attr)
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "missing regex attribute"), true, true);
-		goto done;
-	}
-	tagname_attr = xmlGetNoNsProp(commandInfo->element, TAGNAME_ATTR);
-	if (!tagname_attr)
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "missing tagname attribute"), true, true);
-		goto done;
-	}
-	select_attr = xmlGetNoNsProp(commandInfo->element, BAD_CAST "select");
-	if (!select_attr)
-		select_attr = BAD_CAST XPL_STRDUP(".");
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, KEEPDELIMITER_ATTR, &keepdelimiter, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		goto done;
-	}
-	if (keepdelimiter)
-	{
-		delimitertagname_attr = xmlGetNoNsProp(commandInfo->element, DELIMITERTAGNAME_ATTR);
-		if (!delimitertagname_attr)
-			delimitertagname_attr = tagname_attr;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, KEEPEMPTYTAGS_ATTR, &keepemptytags, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		goto done;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, REPEAT_ATTR, &repeat, true)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		goto done;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, UNIQUE_ATTR, &unique, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		goto done;
-	}
-	if (unique)
+	if (params->keep_delimiter && !params->delimiter_tagname.ncname)
+		params->delimiter_tagname = params->tagname;
+	if (params->unique)
 		unique_hash = xmlHashCreate(16);
 
-	if ((onig_ret_code = onig_new(&regex, regex_attr, regex_attr + xmlStrlen(regex_attr),
+	if ((onig_ret_code = onig_new(&regex, params->regex, params->regex + xmlStrlen(params->regex),
 		ONIG_OPTION_NONE, ONIG_ENCODING_UTF8, ONIG_SYNTAX_PERL_NG, &err_info)) != ONIG_NORMAL)
 	{
 		if (!onig_error_code_to_str(onig_err_str, onig_ret_code))
@@ -115,112 +132,79 @@ void xplCmdRegexSplitEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result
 		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "Oniguruma cannot create search region"), true, true);
 		goto done;
 	}
-	EXTRACT_NS_AND_TAGNAME(tagname_attr, ns, tagname, commandInfo->element);
-	EXTRACT_NS_AND_TAGNAME(delimitertagname_attr, delim_ns, delimitertagname, commandInfo->element);
 
-	sel = xplSelectNodes(commandInfo, commandInfo->element, select_attr);
-	if (!sel)
+	content = commandInfo->content;
+	if (!content || !*content)
 	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "invalid select XPath expression \"%s\"", select_attr), true, true);
-		goto done;
-	}
-	if (sel->type != XPATH_NODESET)
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "select XPath expression \"%s\" evaluated to non-nodeset value", select_attr), true, true);
-		goto done;
-	}
-	if (!sel->nodesetval)
-	{
-		ASSIGN_RESULT(NULL, false, true);
-		goto done;
-	}
-	for (i = 0; i < (size_t) sel->nodesetval->nodeNr; i++)
-	{
-		if ((sel->nodesetval->nodeTab[i]->type != XML_ELEMENT_NODE) && (sel->nodesetval->nodeTab[i]->type != XML_ATTRIBUTE_NODE))
-			continue;
-		content = xmlNodeListGetString(sel->nodesetval->nodeTab[i]->doc, sel->nodesetval->nodeTab[i]->children, 1);
-		if (!content || !*content)
+		if (params->keep_empty_tags)
 		{
-			if (keepemptytags)
+			cur = xmlNewDocNode(commandInfo->element->doc, params->tagname.ns, params->tagname.ncname, NULL);
+			APPEND()
+		}
+		ASSIGN_RESULT(ret, params->repeat, true);
+		goto done;
+	}
+
+	content_end = content + xmlStrlen(content);
+	match = (onig_search(regex, content, content_end, content, content_end, region, 0) != ONIG_MISMATCH);
+	if (!match)
+	{
+		cur = xmlNewDocNode(commandInfo->element->doc, params->tagname.ns, params->tagname.ncname, NULL);
+		cur->children = xmlNewDocText(commandInfo->element->doc, NULL);
+		cur->children->content = content;
+		APPEND()
+	} else {
+		prev_boundary = start = content;
+		while (match && *start)
+		{
+			end = content + (size_t) region->beg[0];
+			if ((end > prev_boundary) || params->keep_empty_tags)
 			{
-				cur = xmlNewDocNode(commandInfo->element->doc, ns, tagname, NULL);
+				tmp = *end;
+				*end = 0;
+				if (!params->unique || (xmlHashAddEntry(unique_hash, prev_boundary, &marker) != -1))
+				{
+					cur = xmlNewDocNode(commandInfo->element->doc, params->tagname.ns, params->tagname.ncname, prev_boundary);
+					APPEND()
+				}
+				*end = tmp;
+			}
+			end = content + (size_t) region->end[0];
+			if (params->keep_delimiter)
+			{
+				start = content + (size_t) region->beg[0];
+				tmp = *end;
+				*end = 0;
+				cur = xmlNewDocNode(commandInfo->element->doc, params->delimiter_tagname.ns, params->delimiter_tagname.ncname, start);
+				*end = tmp;
 				APPEND()
 			}
-			if (content) XPL_FREE(content);
-			continue;
+			if ((region->beg[0] == region->end[0]) && *start) /* zero-length match */
+				start = end + xstrGetOffsetToNextUTF8Char(end);
+			else
+				start = end;
+			prev_boundary = end;
+			match = (onig_search(regex, content, content_end, start, content_end, region, 0) != ONIG_MISMATCH);
 		}
-		content_end = content + xmlStrlen(content);
-		match = (onig_search(regex, content, content_end, content, content_end, region, 0) != ONIG_MISMATCH);
-		if (!match)
+		if (*prev_boundary || params->keep_empty_tags)
 		{
-			cur = xmlNewDocNode(commandInfo->element->doc, ns, tagname, NULL);
-			cur->children = xmlNewDocText(commandInfo->element->doc, NULL);
-			cur->children->content = content;
-			APPEND()
-		} else {
-			prev_boundary = start = content;
-			while (match && *start)
+			if (!*prev_boundary)
+				prev_boundary = NULL;
+			if (!params->unique || (xmlHashAddEntry(unique_hash, prev_boundary, &marker) != -1))
 			{
-				end = content + (size_t) region->beg[0];
-				if ((end > prev_boundary) || keepemptytags)
-				{
-					tmp = *end;
-					*end = 0;
-					if (!unique || (xmlHashAddEntry(unique_hash, prev_boundary, &marker) != -1))
-					{
-						cur = xmlNewDocNode(commandInfo->element->doc, ns, tagname, prev_boundary);
-						APPEND()
-					}
-					*end = tmp;
-				}
-				end = content + (size_t) region->end[0];
-				if (keepdelimiter)
-				{
-					start = content + (size_t) region->beg[0];
-					tmp = *end;
-					*end = 0;
-					cur = xmlNewDocNode(commandInfo->element->doc, delim_ns, delimitertagname, start);
-					*end = tmp;
-					APPEND()
-				}
-				if ((region->beg[0] == region->end[0]) && *start) /* zero-length match */
-					start = end + xstrGetOffsetToNextUTF8Char(end);
-				else
-					start = end;
-				prev_boundary = end;
-				match = (onig_search(regex, content, content_end, start, content_end, region, 0) != ONIG_MISMATCH);
-			} /* inner loop */
-			if (*prev_boundary || keepemptytags)
-			{
-				if (!*prev_boundary)
-					prev_boundary = NULL;
-				if (!unique || (xmlHashAddEntry(unique_hash, prev_boundary, &marker) != -1))
-				{
-					cur = xmlNewDocNode(commandInfo->element->doc, ns, tagname, prev_boundary);
-					APPEND()
-				}
-			} 
-			XPL_FREE(content);
-		} /* first match found */
-	} /* outer loop */
-	ASSIGN_RESULT(ret, repeat, true);
+				cur = xmlNewDocNode(commandInfo->element->doc, params->tagname.ns, params->tagname.ncname, prev_boundary);
+				APPEND()
+			}
+		}
+	}
+	ASSIGN_RESULT(ret, params->repeat, true);
 done:
-	if (select_attr)
-		XPL_FREE(select_attr);
-	if (regex_attr)
-		XPL_FREE(regex_attr);
-	if (delimitertagname_attr && (delimitertagname_attr != tagname_attr))
-		XPL_FREE(delimitertagname_attr);
-	if (tagname_attr)
-		XPL_FREE(tagname_attr);
+	if (params->tagname.ncname == params->delimiter_tagname.ncname)
+		params->delimiter_tagname.ncname = NULL;
 	if (unique_hash)
 		xmlHashFree(unique_hash, NULL);
 	if (regex)
 		onig_free(regex);
 	if (region)
 		onig_region_free(region, 1);
-	if (sel)
-		xmlXPathFreeObject(sel);
 }
-
-xplCommand xplRegexSplitCommand = { NULL, xplCmdRegexSplitEpilogue };
