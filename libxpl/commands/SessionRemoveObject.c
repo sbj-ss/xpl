3d0080dd862dee0c4a7764c77ccc0b974abdb177
dennis <dennis.podkovyrin@gmail.com>
2020-12-16 20:01:49 +0700

    reworked :session-remove-object

--- libxpl/commands/SessionRemoveObject.c
+++ libxpl/commands/SessionRemoveObject.c
@@ -6,31 +6,47 @@
 
 void xplCmdSessionRemoveObjectEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+typedef struct _xplCmdSessionRemoveObjectParams
+{
+	xmlChar *name;
+	bool thread_local;
+} xplCmdSessionRemoveObjectParams, *xplCmdSessionRemoveObjectParamsPtr;
+
+static const xplCmdSessionRemoveObjectParams params_stencil =
+{
+	.name = NULL,
+	.thread_local = false
+};
+
+xplCommand xplSessionRemoveObjectCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdSessionRemoveObjectEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdSessionRemoveObjectParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "name",
+			.type = XPL_CMD_PARAM_TYPE_STRING,
+			.required = true,
+			.value_stencil = &params_stencil.name
+		}, {
+			.name = BAD_CAST "threadlocal",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.thread_local
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdSessionRemoveObjectEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define NAME_ATTR (BAD_CAST "name")
-#define THREADLOCAL_ATTR (BAD_CAST "threadlocal")
-	xmlChar *name_attr;
-	bool threadlocal;
-	xmlNodePtr error;
+	xplCmdSessionRemoveObjectParamsPtr params = (xplCmdSessionRemoveObjectParamsPtr) commandInfo->params;
 
-	name_attr = xmlGetNoNsProp(commandInfo->element, NAME_ATTR);
-	if (!name_attr)
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "no name attribute specified"), true, true);
-		return;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, THREADLOCAL_ATTR, &threadlocal, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		goto done;
-	}
-	if (threadlocal)
-		name_attr = xstrAppendThreadIdToString(name_attr, xprGetCurrentThreadId());
-	xplSessionRemoveObject(commandInfo->document->main->session, name_attr);
+	if (params->thread_local)
+		params->name = xstrAppendThreadIdToString(params->name, xprGetCurrentThreadId());
+	xplSessionRemoveObject(commandInfo->document->main->session, params->name);
 	ASSIGN_RESULT(NULL, false, true);
-done:
-	if (name_attr) XPL_FREE(name_attr);
 }
-
-xplCommand xplSessionRemoveObjectCommand = { NULL, xplCmdSessionRemoveObjectEpilogue };
