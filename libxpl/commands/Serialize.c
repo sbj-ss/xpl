75a671b275ecd054a3e8b72308bb4c683aea03e7
dennis <dennis.podkovyrin@gmail.com>
2020-12-16 13:51:34 +0700

    reworked :serialize

--- libxpl/commands/Serialize.c
+++ libxpl/commands/Serialize.c
@@ -5,37 +5,52 @@
 
 void xplCmdSerializeEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+typedef struct _xplCmdSerializeParams
+{
+	xmlXPathObjectPtr select;
+} xplCmdSerializeParams, *xplCmdSerializeParamsPtr;
+
+static const xplCmdSerializeParams params_stencil =
+{
+	.select = NULL
+};
+
+xplCommand xplSerializeCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdSerializeEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdSerializeParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "select",
+			.type = XPL_CMD_PARAM_TYPE_XPATH,
+			.extra.xpath_type = XPL_CMD_PARAM_XPATH_TYPE_NODESET,
+			.value_stencil = &params_stencil.select
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdSerializeEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define SELECT_ATTR (BAD_CAST "select")
-	xmlChar *select_attr = NULL;
-	xmlXPathObjectPtr obj = NULL;
+	xplCmdSerializeParamsPtr params = (xplCmdSerializeParamsPtr) commandInfo->params;
 	xmlChar *txt;
 	xmlNodePtr ret;
 
-	select_attr = xmlGetNoNsProp(commandInfo->element, SELECT_ATTR);
-	if (select_attr)
+	if (params->select)
 	{
-		obj = xplSelectNodes(commandInfo, commandInfo->element, select_attr);
-		if (!obj)
+		if (params->select->type != XPATH_NODESET)
 		{
-			ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "invalid select XPath expression (%s)", select_attr), true, true);
-			goto done;
+			ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "select XPath expression (%s) evaluated to non-nodeset value", params->select), true, true);
+			return;
 		}
-		if (obj->type != XPATH_NODESET) 
-		{
-			ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "select XPath expression (%s) evaluated to non-nodeset value", select_attr), true, true);
-			goto done;
-		}
-		txt = serializeNodeSet(obj->nodesetval);
+		txt = serializeNodeSet(params->select->nodesetval);
 	} else
 		txt = serializeNodeList(commandInfo->element->children);
 	ret = xmlNewDocText(commandInfo->element->doc, NULL);
 	ret->content = txt;
 	ASSIGN_RESULT(ret, false, true);
-done:
-	if (select_attr) XPL_FREE(select_attr);
-	if (obj) xmlXPathFreeObject(obj);
 }
-
-xplCommand xplSerializeCommand = { NULL, xplCmdSerializeEpilogue };
