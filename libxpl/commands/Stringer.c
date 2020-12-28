1f5bf41b68e9b95802ee6ef50e32b84ff78697fc
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 03:36:37 +0700

    reworked :stringer

--- libxpl/commands/Stringer.c
+++ libxpl/commands/Stringer.c
@@ -5,87 +5,91 @@
 
 void xplCmdStringerEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
-void xplCmdStringerEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
+typedef struct _xplCmdStringerParams
 {
-#define SELECT_ATTR (BAD_CAST "select")
-#define DELIMITER_ATTR (BAD_CAST "delimiter")
-#define START_DELIMITER_ATTR (BAD_CAST "startdelimiter")
-#define END_DELIMITER_ATTR (BAD_CAST "enddelimiter")
-#define UNIQUE_ATTR (BAD_CAST "unique")
-#define KEEPEMPTYTAGS_ATTR (BAD_CAST "keepemptytags")
-/* Lucifer compatibility */
-# define DELIMETER_ATTR (BAD_CAST "delimeter")
-# define START_DELIMETER_ATTR (BAD_CAST "startdelimeter")
-# define END_DELIMETER_ATTR (BAD_CAST "enddelimeter")
-
-	xmlChar *select_attr = NULL;
-	xmlChar *delimiter_attr = NULL;
-	xmlChar *start_delimiter_attr = NULL;
-	xmlChar *end_delimiter_attr = NULL;
+	xmlXPathObjectPtr select;
+	xmlChar *delimiter;
+	xmlChar *start_delimiter;
+	xmlChar *end_delimiter;
 	bool unique;
 	bool keep_empty_tags;
-	xmlXPathObjectPtr sel = NULL;
-	xmlNodePtr cur, ret = NULL, error;
+} xplCmdStringerParams, *xplCmdStringerParamsPtr;
+
+static const xplCmdStringerParams params_stencil =
+{
+	.select = NULL,
+	.delimiter = NULL,
+	.start_delimiter = NULL,
+	.end_delimiter = NULL,
+	.unique = false,
+	.keep_empty_tags = false
+};
+
+static xmlChar* delimiter_aliases[] = { BAD_CAST "delimeter", NULL };
+static xmlChar* start_delimiter_aliases[] = { BAD_CAST "startdelimeter", NULL };
+static xmlChar* end_delimiter_aliases[] = { BAD_CAST "enddelimeter", NULL };
+
+xplCommand xplStringerCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdStringerEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdStringerParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "select",
+			.type = XPL_CMD_PARAM_TYPE_XPATH,
+			.extra.xpath_type = XPL_CMD_PARAM_XPATH_TYPE_NODESET,
+			.value_stencil = &params_stencil.select
+		}, {
+			.name = BAD_CAST "delimiter",
+			.type = XPL_CMD_PARAM_TYPE_STRING,
+			.aliases = delimiter_aliases,
+			.value_stencil = &params_stencil.delimiter
+		}, {
+			.name = BAD_CAST "startdelimiter",
+			.type = XPL_CMD_PARAM_TYPE_STRING,
+			.aliases = start_delimiter_aliases,
+			.value_stencil = &params_stencil.start_delimiter
+		}, {
+			.name = BAD_CAST "enddelimiter",
+			.type = XPL_CMD_PARAM_TYPE_STRING,
+			.aliases = end_delimiter_aliases,
+			.value_stencil = &params_stencil.end_delimiter
+		}, {
+			.name = BAD_CAST "unique",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.unique
+		}, {
+			.name = BAD_CAST "keepemptytags",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.keep_empty_tags
+		}, {
+			.name = NULL
+		}
+	}
+};
+
+void xplCmdStringerEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
+{
+	xplCmdStringerParamsPtr params = (xplCmdStringerParamsPtr) commandInfo->params;
+	xmlNodePtr cur, ret = NULL;
 	xmlHashTablePtr unique_hash = NULL;
 	size_t i = 0;
 	xmlChar *cur_str, *ret_str = NULL, *ret_str_pos = NULL, *tail_pos = NULL;
-	size_t delim_len = 0, sd_len = 0, ed_len = 0, delims_len, cur_len, ret_len = 0;
+	size_t delim_len, sd_len, ed_len, delims_len, cur_len, ret_len = 0;
 
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, UNIQUE_ATTR, &unique, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, KEEPEMPTYTAGS_ATTR, &keep_empty_tags, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	select_attr = xmlGetNoNsProp(commandInfo->element, SELECT_ATTR);
-	delimiter_attr = xmlGetNoNsProp(commandInfo->element, DELIMITER_ATTR);
-	start_delimiter_attr = xmlGetNoNsProp(commandInfo->element, START_DELIMITER_ATTR);
-	end_delimiter_attr = xmlGetNoNsProp(commandInfo->element, END_DELIMITER_ATTR);
-	if (cfgLuciferCompat)
-	{
-		if (!delimiter_attr)
-			delimiter_attr = xmlGetNoNsProp(commandInfo->element, DELIMETER_ATTR);
-		if (!start_delimiter_attr)
-			start_delimiter_attr = xmlGetNoNsProp(commandInfo->element, START_DELIMETER_ATTR);
-		if (!end_delimiter_attr)
-			end_delimiter_attr = xmlGetNoNsProp(commandInfo->element, END_DELIMETER_ATTR);
-	}
-	if (delimiter_attr)
-		delim_len = xmlStrlen(delimiter_attr);
-	if (start_delimiter_attr)
-		sd_len = xmlStrlen(start_delimiter_attr);
-	if (end_delimiter_attr)
-		ed_len = xmlStrlen(end_delimiter_attr);
+	delim_len = params->delimiter? xmlStrlen(params->delimiter): 0;
+	sd_len = params->start_delimiter? xmlStrlen(params->start_delimiter): 0;
+	ed_len = params->end_delimiter? xmlStrlen(params->end_delimiter): 0;
 	delims_len = delim_len + sd_len + ed_len;
-	if (unique)
+	if (params->unique)
 		unique_hash = xmlHashCreate(16);
 
-	if (select_attr)
-	{
-		sel = xplSelectNodes(commandInfo, commandInfo->element, select_attr);
-		if (sel)
-		{
-			if (sel->type == XPATH_NODESET)
-			{
-				if (!sel->nodesetval || !sel->nodesetval->nodeNr) /* no nodes */
-				{
-					ASSIGN_RESULT(NULL, false, true);
-					goto done;
-				}
-				cur = sel->nodesetval->nodeTab[0];
-			} else {
-				ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "select XPath expression \"%s\" evaluated to non-nodeset value", select_attr), true, true);
-				goto done;
-			}
-		} else {
-			ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "invalid select XPath expression (%s)", select_attr), true, true);
-			goto done;
-		}
-	} else
+	if (params->select)
+		cur = params->select->nodesetval->nodeTab[0];
+	else
 		cur = commandInfo->element->children;
 
 	while (cur) /* unified cycle */
@@ -94,44 +98,45 @@ void xplCmdStringerEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 		if ((cur->type == XML_ELEMENT_NODE) || (cur->type == XML_ATTRIBUTE_NODE))
 		{
 			cur_str = xmlNodeListGetString(cur->doc, cur->children, 1);
-			if ((cur_str && *cur_str) || keep_empty_tags)
+			if ((cur_str && *cur_str) || params->keep_empty_tags)
 			{
-				if (!unique || (xmlHashAddEntry(unique_hash, cur_str, (void*) 883) != - 1)) 
+				if (!params->unique || (xmlHashAddEntry(unique_hash, cur_str, (void*) 883) != - 1))
 				{
 					cur_len = xmlStrlen(cur_str);
 					ret_str_pos = (xmlChar*) ret_len; // remember previous offset
 					ret_len += cur_len + delims_len;
 					ret_str = (xmlChar*) XPL_REALLOC(ret_str, ret_len + 1);
 					ret_str_pos += (size_t) ret_str; // start of current part
-					if (start_delimiter_attr)
+					if (params->start_delimiter)
 					{
-						memcpy(ret_str_pos, start_delimiter_attr, sd_len);
+						memcpy(ret_str_pos, params->start_delimiter, sd_len);
 						ret_str_pos += sd_len;
 					}
 					memcpy(ret_str_pos, cur_str, cur_len);
 					ret_str_pos += cur_len;
-					if (end_delimiter_attr)
+					if (params->end_delimiter)
 					{
-						memcpy(ret_str_pos, end_delimiter_attr, ed_len);
+						memcpy(ret_str_pos, params->end_delimiter, ed_len);
 						ret_str_pos += ed_len;
 					}
-					if (delimiter_attr)
+					if (params->delimiter)
 					{
-						memcpy(ret_str_pos, delimiter_attr, delim_len);
+						memcpy(ret_str_pos, params->delimiter, delim_len);
 						tail_pos = ret_str_pos; // mark the point to clear the trailing delimiter
 						ret_str_pos += delim_len;
 					}
 					*ret_str_pos = 0;
 				}
 			}
-			if (cur_str) XPL_FREE(cur_str);
-		}
+			if (cur_str)
+				XPL_FREE(cur_str);
+		} // TODO warn? TODO text?
 		// advance to next node
-		if (sel)
+		if (params->select)
 		{
-			if (++i >= (size_t) sel->nodesetval->nodeNr)
+			if (++i >= (size_t) params->select->nodesetval->nodeNr)
 				break;
-			cur = sel->nodesetval->nodeTab[i];
+			cur = params->select->nodesetval->nodeTab[i];
 		} else
 			cur = cur->next;
 	}
@@ -143,19 +148,6 @@ void xplCmdStringerEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 		ret->content = ret_str;
 	}
 	ASSIGN_RESULT(ret, false, true);
-done:
-	if (select_attr)
-		XPL_FREE(select_attr);
-	if (delimiter_attr)
-		XPL_FREE(delimiter_attr);
-	if (start_delimiter_attr)
-		XPL_FREE(start_delimiter_attr);
-	if (end_delimiter_attr)
-		XPL_FREE(end_delimiter_attr);
-	if (sel)
-		xmlXPathFreeObject(sel);
 	if (unique_hash)
 		xmlHashFree(unique_hash, NULL);
 }
-
-xplCommand xplStringerCommand = { NULL, xplCmdStringerEpilogue };
