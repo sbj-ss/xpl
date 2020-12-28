ef172d3c2d2264530fadd5c1966b65f8e0da70e2
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 03:00:24 +0700

    reworked :stack-pop

--- libxpl/commands/StackPop.c
+++ libxpl/commands/StackPop.c
@@ -2,18 +2,37 @@
 
 void xplCmdStackPopEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+typedef struct _xplCmdStackPopParams
+{
+	bool repeat;
+} xplCmdStackPopParams, *xplCmdStackPopParamsPtr;
+
+static const xplCmdStackPopParams params_stencil =
+{
+	.repeat = true
+};
+
+xplCommand xplStackPopCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdStackPopEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdStackPopParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdStackPopEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define REPEAT_ATTR (BAD_CAST "repeat")
-	xmlChar *repeat_attr = NULL;
-	bool repeat = true;
+	xplCmdStackPopParamsPtr params = (xplCmdStackPopParamsPtr) commandInfo->params;
 
-	repeat_attr = xmlGetNoNsProp(commandInfo->element, REPEAT_ATTR);
-	if (repeat_attr && !xmlStrcasecmp(repeat_attr, BAD_CAST "false"))
-		repeat = false;
-	ASSIGN_RESULT(xplPopFromDocStack(commandInfo->document, commandInfo->element->parent), repeat, true);
-	if (repeat_attr)
-		XPL_FREE(repeat_attr);
+	ASSIGN_RESULT(xplPopFromDocStack(commandInfo->document, commandInfo->element->parent), params->repeat, true);
 }
-
-xplCommand xplStackPopCommand = { NULL, xplCmdStackPopEpilogue };
