6298f07eae726a4a2c5530a0de3ae4be96d8c2a9
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 02:50:31 +0700

    reworked :stack-clear

--- libxpl/commands/StackClear.c
+++ libxpl/commands/StackClear.c
@@ -2,10 +2,16 @@
 
 void xplCmdStackClearEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+xplCommand xplStackClearCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdStackClearEpilogue,
+	.flags = 0,
+	.params_stencil = NULL
+};
+
 void xplCmdStackClearEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
 	xplClearDocStack(commandInfo->document);
 	ASSIGN_RESULT(NULL, false, true);
 }
-
-xplCommand xplStackClearCommand = { NULL, xplCmdStackClearEpilogue };
