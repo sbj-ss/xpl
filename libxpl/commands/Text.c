f41eab87ae9f4f926930b83291bfeb2bd7ab0f68
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 05:22:45 +0700

    reworked :text

--- libxpl/commands/Text.c
+++ libxpl/commands/Text.c
@@ -1,32 +1,48 @@
 #include <libxpl/xplcore.h>
 #include <libxpl/xpltree.h>
 
+void xplCmdTextPrologue(xplCommandInfoPtr commandInfo);
 void xplCmdTextEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
-void xplCmdTextEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
+typedef struct _xplCmdTextParams
 {
-#define REPEAT_ATTR (BAD_CAST "repeat")
-
 	bool repeat;
-	xmlNodePtr error;
+} xplCmdTextParams, *xplCmdTextParamsPtr;
 
-	commandInfo->document->indent_spinlock--;
-	if (!(commandInfo->element->type & XPL_NODE_DELETION_MASK))
-	{
-		if ((error = xplDecodeCmdBoolParam(commandInfo->element, REPEAT_ATTR, &repeat, false)))
-		{
-			ASSIGN_RESULT(error, true, true);
-			return;
-		} else
-			ASSIGN_RESULT(xplDetachContent(commandInfo->element), repeat, true);
-	} else
-		ASSIGN_RESULT(NULL, false, false);
-}
+static const xplCmdTextParams params_stencil =
+{
+	.repeat = false
+};
 
 xplCommand xplTextCommand = {
 	.prologue = NULL,
 	.epilogue = xplCmdTextEpilogue,
-	.initializer = NULL,
-	.finalizer = NULL,
-	.flags = XPL_CMD_FLAG_CONTENT_SAFE
+	.flags = XPL_CMD_FLAG_CONTENT_SAFE | XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdTextParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = NULL
+		}
+	}
 };
+
+void xplCmdTextPrologue(xplCommandInfoPtr commandInfo)
+{
+	commandInfo->document->indent_spinlock++;
+}
+
+void xplCmdTextEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
+{
+	xplCmdTextParamsPtr params = (xplCmdTextParamsPtr) commandInfo->params;
+
+	commandInfo->document->indent_spinlock--;
+	if (!(commandInfo->element->type & XPL_NODE_DELETION_MASK))
+		ASSIGN_RESULT(xplDetachContent(commandInfo->element), params->repeat, true);
+	else
+		ASSIGN_RESULT(NULL, false, false);
+}
