cfade3b9c9329efa6e1e3b81d3e5f4894cc878df
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 03:01:15 +0700

    reworked :stack-push

--- libxpl/commands/StackPush.c
+++ libxpl/commands/StackPush.c
@@ -2,10 +2,16 @@
 
 void xplCmdStackPushEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+xplCommand xplStackPushCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdStackPushEpilogue,
+	.flags = 0,
+	.params_stencil = NULL
+};
+
 void xplCmdStackPushEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
 	xplPushToDocStack(commandInfo->document, commandInfo->element);
 	ASSIGN_RESULT(NULL, false, true);
 }
-
-xplCommand xplStackPushCommand = { NULL, xplCmdStackPushEpilogue };
