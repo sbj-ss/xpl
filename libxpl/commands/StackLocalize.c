e7e30167913699faac20d62fa5f2757599ea16eb
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 02:57:03 +0700

    reworked :stack-localize

--- libxpl/commands/StackLocalize.c
+++ libxpl/commands/StackLocalize.c
@@ -4,6 +4,33 @@
 void xplCmdStackLocalizePrologue(xplCommandInfoPtr commandInfo);
 void xplCmdStackLocalizeEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+typedef struct _xplCmdStackLocalizeParams
+{
+	bool repeat;
+} xplCmdStackLocalizeParams, *xplCmdStackLocalizeParamsPtr;
+
+static const xplCmdStackLocalizeParams params_stencil =
+{
+	.repeat = false
+};
+
+xplCommand xplStackLocalizeCommand = {
+	.prologue = xplCmdStackLocalizePrologue,
+	.epilogue = xplCmdStackLocalizeEpilogue,
+	.flags = XPL_CMD_FLAG_CONTENT_SAFE | XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdStackLocalizeParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdStackLocalizePrologue(xplCommandInfoPtr commandInfo)
 {
 	commandInfo->prologue_state = commandInfo->document->stack;
@@ -12,30 +39,13 @@ void xplCmdStackLocalizePrologue(xplCommandInfoPtr commandInfo)
 
 void xplCmdStackLocalizeEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define REPEAT_ATTR (BAD_CAST "repeat")
-	bool repeat;
+	xplCmdStackLocalizeParamsPtr params = (xplCmdStackLocalizeParamsPtr) commandInfo->params;
 	xmlNodePtr old_stack = (xmlNodePtr) commandInfo->prologue_state;
-	xmlNodePtr error;
 
 	xplClearDocStack(commandInfo->document);
 	commandInfo->document->stack = old_stack;
 	if (commandInfo->element->type & XPL_NODE_DELETION_MASK)
-	{
 		ASSIGN_RESULT(NULL, false, false);
-	} else {
-		if ((error = xplDecodeCmdBoolParam(commandInfo->element, REPEAT_ATTR, &repeat, false)))
-		{
-			ASSIGN_RESULT(error, true, true);
-			return;
-		} else
-			ASSIGN_RESULT(xplDetachContent(commandInfo->element), repeat, true);
-	}
+	else
+		ASSIGN_RESULT(xplDetachContent(commandInfo->element), params->repeat, true);
 }
-
-xplCommand xplStackLocalizeCommand = {
-	.prologue = xplCmdStackLocalizePrologue, 
-	.epilogue = xplCmdStackLocalizeEpilogue,
-	.initializer = NULL,
-	.finalizer = NULL,
-	.flags = XPL_CMD_FLAG_CONTENT_SAFE
-};
