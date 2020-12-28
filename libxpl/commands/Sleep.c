74318d6acf834c7302835b374ccdc02213ef755d
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 02:48:24 +0700

    reworked :sleep

--- libxpl/commands/Sleep.c
+++ libxpl/commands/Sleep.c
@@ -5,43 +5,39 @@
 
 void xplCmdSleepEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+typedef struct _xplCmdSleepParams
+{
+	int delay;
+} xplCmdSleepParams, *xplCmdSleepParamsPtr;
+
+static const xplCmdSleepParams params_stencil =
+{
+	.delay = 0
+};
+
+xplCommand xplSleepCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdSleepEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdSleepParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "delay",
+			.type = XPL_CMD_PARAM_TYPE_INT,
+			.required = true,
+			.value_stencil = &params_stencil.delay
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdSleepEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define DELAY_ATTR (BAD_CAST "delay")
-#define UNTIL_ATTR (BAD_CAST "until")
-	xmlChar* delay_attr = NULL;
-	xmlChar* until_attr = NULL;
+	xplCmdSleepParamsPtr params = (xplCmdSleepParamsPtr) commandInfo->params;
 
-	delay_attr = xmlGetNoNsProp(commandInfo->element, DELAY_ATTR);
-	until_attr = xmlGetNoNsProp(commandInfo->element, UNTIL_ATTR);
-	if (delay_attr && until_attr)
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "both delay and until can't be specified at the same time"), true, true);
-		goto done;
-	}
-	if (!delay_attr && !until_attr)
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "neither delay nor until specified"), true, true);
-		goto done;
-	}
-	if (delay_attr)
-	{
-		int delay;
-		if (sscanf((const char*) delay_attr, "%d", &delay) != 1)
-		{
-			ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "xpl:sleep: non-integer delay value (%s)", delay_attr), true, true);
-			return;
-		}
-		xprSleep(delay);
-	} else if (until_attr) {
-		/* ToDo */
-	}
+	xprSleep(params->delay);
 	ASSIGN_RESULT(NULL, false, true);
-done:
-	if (delay_attr)
-		XPL_FREE(delay_attr);
-	if (until_attr)
-		XPL_FREE(until_attr);
 }
-
-xplCommand xplSleepCommand = { NULL, xplCmdSleepEpilogue };
