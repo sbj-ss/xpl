092da8a8f43625bec641c5d512b6968df07434a1
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 19:36:02 +0700

    reworked :wait-for-threads

--- libxpl/commands/WaitForThreads.c
+++ libxpl/commands/WaitForThreads.c
@@ -2,11 +2,17 @@
 
 void xplCmdWaitForThreadsEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+xplCommand xplWaitForThreadsCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdWaitForThreadsEpilogue,
+	.flags = 0,
+	.params_stencil = NULL
+};
+
 void xplCmdWaitForThreadsEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
 	if (commandInfo->document->threads)
 		xplWaitForChildThreads(commandInfo->document);
 	ASSIGN_RESULT(NULL, false, true);
 }
-
-xplCommand xplWaitForThreadsCommand = { NULL, xplCmdWaitForThreadsEpilogue };
