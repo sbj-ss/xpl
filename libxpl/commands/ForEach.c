092231614db9ccba8093f663f4857dbde59370fd
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 21:18:57 +0700

    reworked :for-each

--- libxpl/commands/ForEach.c
+++ libxpl/commands/ForEach.c
@@ -5,92 +5,77 @@
 void xplCmdForEachPrologue(xplCommandInfoPtr commandInfo);
 void xplCmdForEachEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
-/* A short note for those wanting to replace children with error node inside prologue:
- * 1. the error node must replace the command and not reside inside it
- * 2. double error processing is an error, too
- */
+typedef struct _xplCmdForEachParams
+{
+	xmlXPathObjectPtr select;
+	xmlChar *id;
+	bool repeat;
+} xplCmdForEachParams, *xplCmdForEachParamsPtr;
+
+static const xplCmdForEachParams params_stencil =
+{
+	.select = NULL,
+	.id = NULL,
+	.repeat = false
+};
+
+xplCommand xplForEachCommand = {
+	.prologue = xplCmdForEachPrologue,
+	.epilogue = xplCmdForEachEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_PROLOGUE | XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdForEachParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "select",
+			.type = XPL_CMD_PARAM_TYPE_XPATH,
+			.extra.xpath_type = XPL_CMD_PARAM_XPATH_TYPE_NODESET,
+			.required = true,
+			.value_stencil = &params_stencil.select
+		}, {
+			.name = BAD_CAST "id",
+			.type = XPL_CMD_PARAM_TYPE_STRING,
+			.value_stencil = &params_stencil.id
+		}, {
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdForEachPrologue(xplCommandInfoPtr commandInfo)
 {
-#define SELECT_ATTR (BAD_CAST "select")
-#define ID_ATTR (BAD_CAST "id")
-	xmlChar *select_attr = NULL;
-	xmlChar *id_attr = NULL;
-	xmlXPathObjectPtr sel = NULL;
+	xplCmdForEachParamsPtr params = (xplCmdForEachParamsPtr) commandInfo->params;
 	size_t i;
 	xmlNodePtr cur, tail, repl, ret = NULL;
 	
-	select_attr = xmlGetNoNsProp(commandInfo->element, SELECT_ATTR);
-	if (!select_attr)
+	if (params->select->nodesetval)
 	{
-		ret = NULL;
-		commandInfo->prologue_error = xplCreateErrorNode(commandInfo->element, BAD_CAST "missing select attribute");
-		goto done;
-	}
-	id_attr = xmlGetNoNsProp(commandInfo->element, ID_ATTR);
-
-	sel = xplSelectNodes(commandInfo, commandInfo->element, select_attr);
-	if (sel)
-	{
-		if (sel->type == XPATH_NODESET)
+		for (i = 0; i < (size_t) params->select->nodesetval->nodeNr; i++)
 		{
-			if (sel->nodesetval)
-			{
-				for (i = 0; i < (size_t) sel->nodesetval->nodeNr; i++)
-				{
-					cur = sel->nodesetval->nodeTab[i];
-					if ((cur->type != XML_ELEMENT_NODE) && (cur->type != XML_ATTRIBUTE_NODE))
-						continue;
-					repl = xplReplaceContentEntries(commandInfo->document, id_attr, cur, commandInfo->element->children); 
-					if (!ret)
-						ret = repl;
-					else {
-						tail->next = repl;
-						repl->prev = tail;
-					}
-					tail = xplFindTail(repl);
-				}
+			cur = params->select->nodesetval->nodeTab[i];
+			if ((cur->type != XML_ELEMENT_NODE) && (cur->type != XML_ATTRIBUTE_NODE))
+				continue;
+			repl = xplReplaceContentEntries(commandInfo->document, params->id, cur, commandInfo->element->children);
+			if (!ret)
+				ret = repl;
+			else {
+				tail->next = repl;
+				repl->prev = tail;
 			}
-		} else {
-			ret = NULL;
-			commandInfo->prologue_error = xplCreateErrorNode(commandInfo->element, BAD_CAST "select XPath (%s) evaluated to non-nodeset value", select_attr);
-			goto done;
+			tail = xplFindTail(repl);
 		}
-	} else {
-		ret = NULL;
-		commandInfo->prologue_error = xplCreateErrorNode(commandInfo->element, BAD_CAST "invalid select XPath expression (%s)", select_attr);
-		goto done;
 	}
-done:
 	xplDocDeferNodeListDeletion(commandInfo->document, xplDetachContent(commandInfo->element));
 	xplSetChildren(commandInfo->element, ret);	
-	if (select_attr) XPL_FREE(select_attr);
-	if (id_attr) xmlFree (id_attr);
-	if (sel) xmlXPathFreeObject(sel);
 }
 
 void xplCmdForEachEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define REPEAT_ATTR (BAD_CAST "repeat")
-	bool repeat;
-	xmlNodePtr error;
+	xplCmdForEachParamsPtr params = (xplCmdForEachParamsPtr) commandInfo->params;
 
-	if (commandInfo->prologue_error)
-	{
-		ASSIGN_RESULT(commandInfo->prologue_error, true, true);
-		return;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, REPEAT_ATTR, &repeat, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	ASSIGN_RESULT(xplDetachContent(commandInfo->element), repeat, true);
+	ASSIGN_RESULT(xplDetachContent(commandInfo->element), params->repeat, true);
 }
-
-xplCommand xplForEachCommand = { 
-	.prologue = xplCmdForEachPrologue, 
-	.epilogue = xplCmdForEachEpilogue,
-	.initializer = NULL,
-	.finalizer = NULL,
-	.flags = 0 /* we don't need "content safe flag here */
-};
