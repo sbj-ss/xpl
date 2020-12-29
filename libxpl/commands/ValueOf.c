960d62697bc5011203b1583d2d0af9497e68ba32
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 19:34:20 +0700

    reworked :value-of

--- libxpl/commands/ValueOf.c
+++ libxpl/commands/ValueOf.c
@@ -3,37 +3,42 @@
 
 void xplCmdValueOfEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+typedef struct _xplCmdValueOfParams
+{
+	xmlXPathObjectPtr select;
+} xplCmdValueOfParams, *xplCmdValueOfParamsPtr;
+
+static const xplCmdValueOfParams params_stencil =
+{
+	.select = NULL
+};
+
+xplCommand xplValueOfCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdValueOfEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdValueOfParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "select",
+			.type = XPL_CMD_PARAM_TYPE_XPATH,
+			.required = true,
+			.extra.xpath_type = XPL_CMD_PARAM_XPATH_TYPE_SCALAR,
+			.value_stencil = &params_stencil.select
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdValueOfEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define SELECT_ATTR (BAD_CAST "select")
-	xmlXPathObjectPtr sel = NULL;
-	xmlChar *select_attr;
-	xmlChar *value;
+	xplCmdValueOfParamsPtr params = (xplCmdValueOfParamsPtr) commandInfo->params;
 	xmlNodePtr ret;
 	
-	select_attr = xmlGetNoNsProp(commandInfo->element, SELECT_ATTR);
-	if (!select_attr)
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "missing select attribute"), true, true);
-		return;
-	}
-	 sel = xplSelectNodes(commandInfo, commandInfo->element, select_attr);
-	if (sel)
-	{
-		if ((sel->type == XPATH_BOOLEAN) || (sel->type == XPATH_NUMBER) || (sel->type == XPATH_STRING))
-		{
-			value = xmlXPathCastToString(sel);
-			ret = xmlNewDocText(commandInfo->document->document, NULL);
-			ret->content = value;
-			ASSIGN_RESULT(ret, false, true);
-		} else {
-			ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "select XPath expression \"%s\" evaluated to non-scalar value", select_attr), true, true);
-		}
-		xmlXPathFreeObject(sel);
-	} else {
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "invalid select XPath expression \"%s\"", select_attr), true, true);
-	}
-	XPL_FREE(select_attr);
+	ret = xmlNewDocText(commandInfo->document->document, NULL);
+	ret->content = xmlXPathCastToString(params->select);
+	ASSIGN_RESULT(ret, false, true);
 }
-
-xplCommand xplValueOfCommand = { NULL, xplCmdValueOfEpilogue };
