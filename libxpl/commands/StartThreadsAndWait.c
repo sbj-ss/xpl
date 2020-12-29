65ce1c21a33804846d2b0989bb5dcb91d2889b9e
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 21:27:32 +0700

    reworked :start-threads-and-wait

--- libxpl/commands/StartThreadsAndWait.c
+++ libxpl/commands/StartThreadsAndWait.c
@@ -2,23 +2,42 @@
 
 void xplCmdStartThreadsAndWaitEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
-#define REPEAT_ATTR (BAD_CAST "repeat")
-void xplCmdStartThreadsAndWaitEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
+typedef struct _xplCmdStartThreadsAndWaitParams
 {
 	bool repeat;
-	xmlNodePtr error;
+} xplCmdStartThreadsAndWaitParams, *xplCmdStartThreadsAndWaitParamsPtr;
 
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, REPEAT_ATTR, &repeat, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
+static const xplCmdStartThreadsAndWaitParams params_stencil =
+{
+	.repeat = false
+};
+
+xplCommand xplStartThreadsAndWaitCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdStartThreadsAndWaitEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdStartThreadsAndWaitParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = NULL
+		}
 	}
+};
+
+void xplCmdStartThreadsAndWaitEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
+{
+	xplCmdStartThreadsAndWaitParamsPtr params = (xplCmdStartThreadsAndWaitParamsPtr) commandInfo->params;
+
 	if (commandInfo->document->has_suspended_threads)
 	{
 		xplStartDelayedThreads(commandInfo->document);
 		xplWaitForChildThreads(commandInfo->document);
 	}
-	ASSIGN_RESULT(xplDetachContent(commandInfo->element), repeat, true);
+	ASSIGN_RESULT(xplDetachContent(commandInfo->element), params->repeat, true);
 }
-
-xplCommand xplStartThreadsAndWaitCommand = { NULL, xplCmdStartThreadsAndWaitEpilogue };
