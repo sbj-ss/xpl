98e5bc271033e00873a387d580cc7ae3682ff0ce
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 22:47:18 +0700

    reworked :unstringer

--- libxpl/commands/Unstringer.c
+++ libxpl/commands/Unstringer.c
@@ -7,29 +7,122 @@
 
 void xplCmdUnstringerEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
-typedef struct _UnstringerContext
+typedef struct _xplCmdUnstringerParams
 {
+	xmlXPathObjectPtr select;
 	xmlChar* delimiter;
 	xmlChar* start_delimiter;
 	xmlChar* end_delimiter;
-	xmlChar* tag_name;
-	xmlChar* delimiter_tag_name;
+	xplQName tag_name;
+	xplQName delimiter_tag_name;
 	bool unique;
 	bool keep_delimiter;
 	bool multi_delimiter;
 	bool keep_empty_tags;
+	bool repeat;
+} xplCmdUnstringerParams, *xplCmdUnstringerParamsPtr;
 
+static const xplCmdUnstringerParams params_stencil =
+{
+	.select = NULL,
+	.delimiter = NULL,
+	.start_delimiter = NULL,
+	.end_delimiter = NULL,
+	.tag_name = { NULL, NULL },
+	.delimiter_tag_name = { NULL, NULL },
+	.unique = false,
+	.keep_delimiter = false,
+	.multi_delimiter = false,
+	.keep_empty_tags = false,
+	.repeat = true
+};
+
+static xmlChar* delimiter_aliases[] = { BAD_CAST "delim", BAD_CAST "delimeter", NULL };
+static xmlChar* start_delimiter_aliases[] = { BAD_CAST "startdelimeter", NULL };
+static xmlChar* end_delimiter_aliases[] = { BAD_CAST "enddelimeter", NULL };
+static xmlChar* keep_delimiter_aliases[] = { BAD_CAST "keepdelimeter", NULL };
+static xmlChar* multi_delimiter_aliases[] = { BAD_CAST "multidelimeter", NULL };
+static xmlChar* delimiter_tag_name_aliases[] = { BAD_CAST "delimetertagname", NULL };
+
+xplCommand xplUnstringerCommand =
+{
+	.prologue = NULL,
+	.epilogue = xplCmdUnstringerEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE | XPL_CMD_FLAG_CONTENT_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdUnstringerParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "select",
+			.type = XPL_CMD_PARAM_TYPE_XPATH,
+			.extra.xpath_type = XPL_CMD_PARAM_XPATH_TYPE_ANY,
+			.value_stencil = &params_stencil.select
+		}, {
+			.name = BAD_CAST "delimiter",
+			.type = XPL_CMD_PARAM_TYPE_STRING,
+			.aliases = delimiter_aliases,
+			.value_stencil = &params_stencil.delimiter
+		}, {
+			.name = BAD_CAST "startdelimiter",
+			.type = XPL_CMD_PARAM_TYPE_STRING,
+			.aliases = start_delimiter_aliases,
+			.value_stencil = &params_stencil.start_delimiter
+		}, {
+			.name = BAD_CAST "enddelimiter",
+			.type = XPL_CMD_PARAM_TYPE_STRING,
+			.aliases = end_delimiter_aliases,
+			.value_stencil = &params_stencil.end_delimiter,
+		}, {
+			.name = BAD_CAST "tagname",
+			.type = XPL_CMD_PARAM_TYPE_QNAME,
+			.required = true,
+			.value_stencil = &params_stencil.tag_name
+		}, {
+			.name = BAD_CAST "delimitertagname",
+			.type = XPL_CMD_PARAM_TYPE_QNAME,
+			.aliases = delimiter_tag_name_aliases,
+			.value_stencil = &params_stencil.delimiter_tag_name
+		}, {
+			.name = BAD_CAST "unique",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.unique
+		}, {
+			.name = BAD_CAST "keepdelimiter",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.aliases = keep_delimiter_aliases,
+			.value_stencil = &params_stencil.keep_delimiter
+		}, {
+			.name = BAD_CAST "multidelimiter",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.aliases = multi_delimiter_aliases,
+			.value_stencil = &params_stencil.multi_delimiter
+		}, {
+			.name = BAD_CAST "keepemptytags",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.keep_empty_tags
+		}, {
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = NULL
+		}
+	}
+};
+
+typedef struct _UnstringerContext
+{
+	xplCmdUnstringerParamsPtr params;
 	xmlChar* input_str;
 	xmlHashTablePtr unique_hash;
 	xmlDocPtr doc;
-	xmlNsPtr ns;
-	xmlNsPtr delimiter_ns;
 } UnstringerContext;
 typedef UnstringerContext *UnstringerContextPtr;
 
 static xmlChar *_getEnd(xmlChar *str)
 {
-	while (*str) str++;
+	while (*str)
+		str++;
 	return str;
 }
 
@@ -79,65 +172,65 @@ static xmlNodePtr _splitBySingle(UnstringerContextPtr ctxt)
 
 	start = ctxt->input_str;
 	ret = tail = NULL;
-	delim_len = ctxt->delimiter? (size_t) xmlStrlen(ctxt->delimiter): 0;
+	delim_len = ctxt->params->delimiter? (size_t) xmlStrlen(ctxt->params->delimiter): 0;
 	while (*start)
 	{
 		if (delim_len)
 		{
-			if (ctxt->multi_delimiter)
-				cur_end = _getNextMulti(start, ctxt->delimiter, &multi_delim_len);
+			if (ctxt->params->multi_delimiter)
+				cur_end = _getNextMulti(start, ctxt->params->delimiter, &multi_delim_len);
 			else
-				cur_end = BAD_CAST xmlStrstr(start, ctxt->delimiter);
+				cur_end = BAD_CAST xmlStrstr(start, ctxt->params->delimiter);
 			if (cur_end)
 			{
 				if (cur_end != start)
 				{
 					tmp = *cur_end;
 					*cur_end = 0;
-					if (!ctxt->unique || (xmlHashAddEntry(ctxt->unique_hash, start, &marker) != -1)) 
+					if (!ctxt->params->unique || (xmlHashAddEntry(ctxt->unique_hash, start, &marker) != -1))
 					{ 
-						cur = xmlNewDocNode(ctxt->doc, ctxt->ns, ctxt->tag_name, start); 
+						cur = xmlNewDocNode(ctxt->doc, ctxt->params->tag_name.ns, ctxt->params->tag_name.ncname, start);
 						APPEND_NODE();
 					} 
 					*cur_end = tmp;
-				} else if (ctxt->keep_empty_tags) {
-					if (!ctxt->unique || (xmlHashAddEntry(ctxt->unique_hash, NULL, &marker) != -1)) 
+				} else if (ctxt->params->keep_empty_tags) {
+					if (!ctxt->params->unique || (xmlHashAddEntry(ctxt->unique_hash, NULL, &marker) != -1))
 					{ 
-						cur = xmlNewDocNode(ctxt->doc, ctxt->ns, ctxt->tag_name, NULL); 
+						cur = xmlNewDocNode(ctxt->doc, ctxt->params->tag_name.ns, ctxt->params->tag_name.ncname, NULL);
 						APPEND_NODE();
 					} 
 				}
-				if (ctxt->keep_delimiter)
+				if (ctxt->params->keep_delimiter)
 				{
-					cur = xmlNewDocNode(ctxt->doc, ctxt->delimiter_ns, ctxt->delimiter_tag_name, NULL); 
+					cur = xmlNewDocNode(ctxt->doc, ctxt->params->delimiter_tag_name.ns, ctxt->params->delimiter_tag_name.ncname, NULL);
 					cur->children = xmlNewDocText(ctxt->doc, NULL); 
-					cur->children->content = xmlStrndup(cur_end, (ctxt->multi_delimiter)? (int) multi_delim_len: (int) delim_len);
+					cur->children->content = xmlStrndup(cur_end, (ctxt->params->multi_delimiter)? (int) multi_delim_len: (int) delim_len);
 					APPEND_NODE();
 				}
-				start = cur_end + ((ctxt->multi_delimiter)? multi_delim_len: delim_len);
-				if (!*start && ctxt->keep_empty_tags)
+				start = cur_end + ((ctxt->params->multi_delimiter)? multi_delim_len: delim_len);
+				if (!*start && ctxt->params->keep_empty_tags)
 				{
-					if (!ctxt->unique || (xmlHashAddEntry(ctxt->unique_hash, NULL, &marker) != -1)) 
+					if (!ctxt->params->unique || (xmlHashAddEntry(ctxt->unique_hash, NULL, &marker) != -1))
 					{ 
-						cur = xmlNewDocNode(ctxt->doc, ctxt->ns, ctxt->tag_name, NULL); 
+						cur = xmlNewDocNode(ctxt->doc, ctxt->params->tag_name.ns, ctxt->params->tag_name.ncname, NULL);
 						APPEND_NODE();
 					} 
 				}
 			} else {
-				if (!ctxt->unique || (xmlHashAddEntry(ctxt->unique_hash, start, &marker) != -1)) 
+				if (!ctxt->params->unique || (xmlHashAddEntry(ctxt->unique_hash, start, &marker) != -1))
 				{ 
-					cur = xmlNewDocNode(ctxt->doc, ctxt->ns, ctxt->tag_name, start); 
+					cur = xmlNewDocNode(ctxt->doc, ctxt->params->tag_name.ns, ctxt->params->tag_name.ncname, start);
 					APPEND_NODE();
 				} 
 				start = _getEnd(start);
 			}
-		} else {
+		} else { // zero delimiter = split by chars
 			cur_end = start + xstrGetOffsetToNextUTF8Char(start);
 			tmp = *cur_end;
 			*cur_end = 0;
-			if (!ctxt->unique || (xmlHashAddEntry(ctxt->unique_hash, start, &marker) != -1)) 
+			if (!ctxt->params->unique || (xmlHashAddEntry(ctxt->unique_hash, start, &marker) != -1))
 			{ 
-				cur = xmlNewDocNode(ctxt->doc, ctxt->ns, ctxt->tag_name, start); 
+				cur = xmlNewDocNode(ctxt->doc, ctxt->params->tag_name.ns, ctxt->params->tag_name.ncname, start);
 				APPEND_NODE();
 			} 
 			*cur_end = tmp;
@@ -145,7 +238,6 @@ static xmlNodePtr _splitBySingle(UnstringerContextPtr ctxt)
 		}
 	}
 	return ret;
-#undef CREATE_NODE
 #undef APPEND_NODE
 }
 
@@ -157,186 +249,78 @@ static xmlNodePtr _splitByCouple(UnstringerContextPtr ctxt)
 
 void xplCmdUnstringerEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 {
-#define SELECT_ATTR (BAD_CAST "select")
-#define UNIQUE_ATTR (BAD_CAST "unique")
-#define TAG_NAME_ATTR (BAD_CAST "tagname")
-#define KEEP_EMPTY_TAGS_ATTR (BAD_CAST "keepemptytags")
-#define REPEAT_ATTR (BAD_CAST "repeat")
-
-#define DELIMITER_ATTR (BAD_CAST "delimiter")
-#define DELIM_ATTR (BAD_CAST "delim")
-#define START_DELIMITER_ATTR (BAD_CAST "startdelimiter")
-#define END_DELIMITER_ATTR  (BAD_CAST "enddelimiter")
-#define DELIMITER_TAG_NAME_ATTR (BAD_CAST "delimitertagname")
-#define KEEP_DELIMITER_ATTR (BAD_CAST "keepdelimiter")
-#define MULTI_DELIMITER_ATTR (BAD_CAST "multidelimiter")
-/* Lucifer compatibility */
-# define DELIMeTER_ATTR (BAD_CAST "delimeter")
-# define START_DELIMeTER_ATTR (BAD_CAST "startdelimeter")
-# define END_DELIMeTER_ATTR  (BAD_CAST "enddelimeter")
-# define DELIMeTER_TAG_NAME_ATTR (BAD_CAST "delimetertagname")
-# define KEEP_DELIMeTER_ATTR (BAD_CAST "keepdelimeter")
-# define MULTI_DELIMeTER_ATTR (BAD_CAST "multidelimeter")
-
-	xmlChar *select_attr = NULL;
-	xmlChar *tag_name_attr = NULL;
-	xmlChar *delimiter_tag_name_attr = NULL;
-	bool repeat = true;
-	xmlNodePtr ret = NULL, error;
 	xmlXPathObjectPtr sel = NULL;
 	UnstringerContext ctxt;
 	size_t i;
-	xmlNodePtr tail, out, cur;
+	xmlNodePtr ret = NULL, tail = NULL, out, cur;
 
 	memset(&ctxt, 0, sizeof(UnstringerContext));
-	tag_name_attr = xmlGetNoNsProp(commandInfo->element, TAG_NAME_ATTR);
-	/* ToDo: check if tag name is valid */
-	if (!tag_name_attr)
-	{
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "missing tagname attribute"), true, true);
-		return;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, UNIQUE_ATTR, &ctxt.unique, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, REPEAT_ATTR, &repeat, true)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, KEEP_EMPTY_TAGS_ATTR, &ctxt.keep_empty_tags, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, KEEP_DELIMITER_ATTR, &ctxt.keep_delimiter, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, MULTI_DELIMITER_ATTR, &ctxt.multi_delimiter, false)))
-	{
-		ASSIGN_RESULT(error, true, true);
-		return;
-	}
-	if (cfgLuciferCompat)
-	{
-		if ((error = xplDecodeCmdBoolParam(commandInfo->element, KEEP_DELIMeTER_ATTR, &ctxt.keep_delimiter, ctxt.keep_delimiter)))
-		{
-			ASSIGN_RESULT(error, true, true);
-			return;
-		}
-		if ((error = xplDecodeCmdBoolParam(commandInfo->element, MULTI_DELIMeTER_ATTR, &ctxt.multi_delimiter, ctxt.multi_delimiter)))
-		{
-			ASSIGN_RESULT(error, true, true);
-			return;
-		}
-	}
-	select_attr = xmlGetNoNsProp(commandInfo->element, SELECT_ATTR);
-	delimiter_tag_name_attr = xmlGetNoNsProp(commandInfo->element, DELIMITER_TAG_NAME_ATTR);
+	ctxt.params = (xplCmdUnstringerParamsPtr) commandInfo->params;
 
-	ctxt.delimiter = xmlGetNoNsProp(commandInfo->element, DELIMITER_ATTR);
-	if (!ctxt.delimiter)
-		ctxt.delimiter = xmlGetNoNsProp(commandInfo->element, DELIM_ATTR);
-	ctxt.start_delimiter = xmlGetNoNsProp(commandInfo->element, START_DELIMITER_ATTR);
-	ctxt.end_delimiter = xmlGetNoNsProp(commandInfo->element, END_DELIMITER_ATTR);	
-	if (cfgLuciferCompat)
-	{
-		if (!ctxt.delimiter)
-			ctxt.delimiter = xmlGetNoNsProp(commandInfo->element, DELIMeTER_ATTR);
-		if (!ctxt.start_delimiter)
-			ctxt.start_delimiter = xmlGetNoNsProp(commandInfo->element, START_DELIMeTER_ATTR);
-		if (!ctxt.end_delimiter)
-			ctxt.end_delimiter = xmlGetNoNsProp(commandInfo->element, END_DELIMeTER_ATTR);
-		if (!ctxt.delimiter_tag_name)
-			ctxt.delimiter_tag_name = xmlGetNoNsProp(commandInfo->element, DELIMeTER_TAG_NAME_ATTR);
-	}
-
-	if (ctxt.delimiter && (ctxt.start_delimiter || ctxt.end_delimiter))
+	if (ctxt.params->delimiter && (ctxt.params->start_delimiter || ctxt.params->end_delimiter))
 	{
 		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "can't perform single delimiter and coupled delimiter splitting at the same time"), true, true);
-		goto done;
+		return;
 	}
-	if (ctxt.multi_delimiter && ctxt.start_delimiter && ctxt.end_delimiter && (xmlStrlen(ctxt.start_delimiter) != xmlStrlen(ctxt.end_delimiter)))
+	if (ctxt.params->multi_delimiter && ctxt.params->start_delimiter && ctxt.params->end_delimiter && (xmlStrlen(ctxt.params->start_delimiter) != xmlStrlen(ctxt.params->end_delimiter)))
 	{
 		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "multi-delimiter splitting is requested, but lengths of start and end delimiters differ"), true, true);
-		goto done;
+		return;
 	}
 
-	if (!select_attr)
-		select_attr = BAD_CAST XPL_STRDUP(".");
-	EXTRACT_NS_AND_TAGNAME(tag_name_attr, ctxt.ns, ctxt.tag_name, commandInfo->element)
-	EXTRACT_NS_AND_TAGNAME(delimiter_tag_name_attr, ctxt.delimiter_ns, ctxt.delimiter_tag_name, commandInfo->element);
-	if (!ctxt.delimiter_tag_name)
-		ctxt.delimiter_tag_name = ctxt.tag_name;
-
+	if (!ctxt.params->delimiter_tag_name.ncname)
+		ctxt.params->delimiter_tag_name = ctxt.params->tag_name;
 	ctxt.doc = commandInfo->element->doc;
-	if (ctxt.unique)
+	if (ctxt.params->unique)
 		ctxt.unique_hash = xmlHashCreate(16);
-	sel = xplSelectNodes(commandInfo, commandInfo->element, select_attr);
-	if (sel)
+	if (ctxt.params->select)
 	{
-		if (sel->type == XPATH_NODESET)
-		{
-			if (sel->nodesetval)
-			{
-				for (i = 0; i < (size_t) sel->nodesetval->nodeNr; i++)
-				{
-					cur = sel->nodesetval->nodeTab[i];
-					if ((cur->type != XML_ELEMENT_NODE && cur->type != XML_ATTRIBUTE_NODE))
-						continue;
-					ctxt.input_str = xmlNodeListGetString(cur->doc, cur->children, 1);
-					if (!ctxt.input_str)
-						continue;
-					if (ctxt.start_delimiter && ctxt.end_delimiter)
-						out = _splitByCouple(&ctxt);
-					else
-						out = _splitBySingle(&ctxt);
-					if (!ret)
-						ret = out;
-					else
-						xplAppendList(tail, out);
-					tail = xplFindTail(out);
-					XPL_FREE(ctxt.input_str);
-				}
-			}
-		} else if (sel->type == XPATH_STRING) {
+		i = 0;
+		cur = NULL;
+		if (ctxt.params->select->type == XPATH_NODESET && ctxt.params->select->nodesetval && ctxt.params->select->nodesetval->nodeNr)
+			cur = ctxt.params->select->nodesetval->nodeTab[0];
+		else if (ctxt.params->select->type == XPATH_STRING) {
 			ctxt.input_str = sel->stringval;
-			if (ctxt.start_delimiter && ctxt.end_delimiter)
+			if (ctxt.params->start_delimiter && ctxt.params->end_delimiter)
 				ret = _splitByCouple(&ctxt);
 			else
 				ret = _splitBySingle(&ctxt);
-		} else {
-			ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "select XPath expression \"%s\" evaluated to neither nodeset nor string value", select_attr), true, true);
-			goto done;
 		}
-	} else {
-		ASSIGN_RESULT(xplCreateErrorNode(commandInfo->element, BAD_CAST "invalid select XPath expression \"%s\"", select_attr), true, true);
-		goto done;
+	} else
+		cur = commandInfo->element->children;
+	while (cur)
+	{
+		if (cur->type == XML_ELEMENT_NODE || cur->type == XML_ATTRIBUTE_NODE)
+			ctxt.input_str = xmlNodeListGetString(cur->doc, cur->children, 1);
+		else if ((cur->type == XML_TEXT_NODE || cur->type == XML_CDATA_SECTION_NODE) && cur->content)
+			ctxt.input_str = BAD_CAST XPL_STRDUP((char*) cur->content);
+		else
+			continue; // TODO warn?
+		if (!ctxt.input_str)
+			continue;
+		if (ctxt.params->start_delimiter && ctxt.params->end_delimiter)
+			out = _splitByCouple(&ctxt);
+		else
+			out = _splitBySingle(&ctxt);
+		if (!ret)
+			ret = out;
+		else
+			xplAppendList(tail, out);
+		tail = xplFindTail(out);
+		XPL_FREE(ctxt.input_str);
+		if (ctxt.params->select)
+		{
+			if (++i >= ctxt.params->select->nodesetval->nodeNr)
+				break;
+			cur = ctxt.params->select->nodesetval->nodeTab[i];
+		} else
+			cur = cur->next;
 	}
 	xplDownshiftNodeListNsDef(ret, commandInfo->element->nsDef);
-	ASSIGN_RESULT(ret, repeat, true);
-done:
-	if (select_attr)
-		XPL_FREE(select_attr);
-	if (tag_name_attr)
-		XPL_FREE(tag_name_attr);
-	if (delimiter_tag_name_attr)
-		XPL_FREE(delimiter_tag_name_attr);
+	ASSIGN_RESULT(ret, ctxt.params->repeat, true);
 
-	if (ctxt.delimiter)
-		XPL_FREE(ctxt.delimiter);
-	if (ctxt.start_delimiter)
-		XPL_FREE(ctxt.start_delimiter);
-	if (ctxt.end_delimiter)
-		XPL_FREE(ctxt.end_delimiter);
 	if (ctxt.unique_hash)
 		xmlHashFree(ctxt.unique_hash, NULL);
-
-	if (sel)
-		xmlXPathFreeObject(sel);
+	if (ctxt.params->delimiter_tag_name.ncname == ctxt.params->tag_name.ncname)
+		ctxt.params->delimiter_tag_name.ncname = NULL;
 }
-
-xplCommand xplUnstringerCommand = { NULL, xplCmdUnstringerEpilogue };
