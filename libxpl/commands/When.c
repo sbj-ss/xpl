737f3d6500b32489ac75a84f6461b47dc3702974
dennis <dennis.podkovyrin@gmail.com>
2020-12-29 20:17:30 +0700

    reworked :when

--- libxpl/commands/When.c
+++ libxpl/commands/When.c
@@ -5,21 +5,54 @@
 void xplCmdWhenPrologue(xplCommandInfoPtr commandInfo);
 void xplCmdWhenEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result);
 
+typedef struct _xplCmdWhenParams
+{
+	xmlChar *test;
+	bool repeat;
+} xplCmdWhenParams, *xplCmdWhenParamsPtr;
+
+static const xplCmdWhenParams params_stencil =
+{
+	.test = NULL,
+	.repeat = false
+};
+
+xplCommand xplWhenCommand =
+{
+	.prologue = xplCmdWhenPrologue,
+	.epilogue = xplCmdWhenEpilogue,
+	.flags = XPL_CMD_FLAG_PARAMS_FOR_PROLOGUE | XPL_CMD_FLAG_PARAMS_FOR_EPILOGUE,
+	.params_stencil = &params_stencil,
+	.stencil_size = sizeof(xplCmdWhenParams),
+	.parameters = {
+		{
+			.name = BAD_CAST "test",
+			.type = XPL_CMD_PARAM_TYPE_STRING,
+			.value_stencil = &params_stencil.test
+		}, {
+			.name = BAD_CAST "repeat",
+			.type = XPL_CMD_PARAM_TYPE_BOOL,
+			.value_stencil = &params_stencil.repeat
+		}, {
+			.name = NULL
+		}
+	}
+};
+
 void xplCmdWhenPrologue(xplCommandInfoPtr commandInfo)
 {
-#define TEST_ATTR (BAD_CAST "test")
-	xmlChar *test_attr = NULL;
+	xplCmdWhenParamsPtr params = (xplCmdWhenParamsPtr) commandInfo->params;
 	xmlNsPtr xpl_ns;
 	xmlNodePtr test_el;
 
-	test_attr = xmlGetNoNsProp(commandInfo->element, TEST_ATTR);
-	if (test_attr)
+	if (params->test)
 	{
 		if (!(xpl_ns = commandInfo->document->root_xpl_ns))
 			xpl_ns = xmlSearchNsByHref(commandInfo->element->doc, commandInfo->element, cfgXplNsUri);
 		test_el = xmlNewDocNode(commandInfo->element->doc, xpl_ns, BAD_CAST "test", NULL);
 		test_el->children = xmlNewDocText(commandInfo->element->doc, NULL);
-		test_el->children->content = test_attr;
+		test_el->children->content = params->test;
+		params->test = NULL;
 		xplPrependList(commandInfo->element->children, test_el);
 	}
 }
@@ -29,14 +62,7 @@ void xplCmdWhenEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 	/* ToDo: :break should be added here and not by :test
 	 * motivation: :choose implies single choice but :when without condition will always succeed.
      */
-#define REPEAT_ATTR (BAD_CAST "repeat")
-	bool repeat;
-	xmlNodePtr error;
+	xplCmdWhenParamsPtr params = (xplCmdWhenParamsPtr) commandInfo->params;
 
-	if ((error = xplDecodeCmdBoolParam(commandInfo->element, REPEAT_ATTR, &repeat, false)))
-		ASSIGN_RESULT(error, true, true);
-	else
-		ASSIGN_RESULT(xplDetachContent(commandInfo->element), repeat, true);
+	ASSIGN_RESULT(xplDetachContent(commandInfo->element), params->repeat, true);
 }
-
-xplCommand xplWhenCommand = { xplCmdWhenPrologue, xplCmdWhenEpilogue };
