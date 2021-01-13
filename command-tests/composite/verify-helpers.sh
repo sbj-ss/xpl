34fe3c375b3369e9883838adefc9a2b59d85d31a
dennis <dennis.podkovyrin@gmail.com>
2021-01-13 15:04:12 +0700

    added bootstrap tests presence checker

---
+++ command-tests/composite/verify-helpers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+missing=0;
+
+for f in $(\
+	grep -Eo '<xpl:[^ />]+' Helpers.xpl\
+	| sort \
+	| uniq \
+	| cut -c 6- \
+	| grep -vF "$(cat bootstrap-exclude.txt)" \
+	| sed -Ee 's/^[a-z]/\u&/; s/-([a-z])/\u\1/g; s/$/.xpl/' \
+); do
+	if [ ! -f ../bootstrap/$f ]; then
+		echo "$f is missing!"
+		((missing++))
+	fi
+done
+
+if [ $missing -gt 0 ]; then
+	echo "$missing test(s) missing!"
+else
+	echo "All bootstrap tests present."
+fi
