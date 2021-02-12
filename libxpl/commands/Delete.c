edc77bcc65d62c963aab3cba19fc957b144766e5
dennis <dennis.podkovyrin@gmail.com>
2021-02-12 09:54:54 +0700

    translated comments in :delete, added warnings

--- libxpl/commands/Delete.c
+++ libxpl/commands/Delete.c
@@ -35,65 +35,22 @@ xplCommand xplDeleteCommand =
 	}
 };
 
-#if 0
-
-inline void printIndent(int indent)
-{
-	int i = indent;
-	while(i--)
-		printf(" ");
-}
-
-void dumpNode(xmlNodePtr cur, int indent);
-void dumpNodeList(xmlNodePtr cur, int indent)
-{
-	while (cur)
-	{
-		dumpNode(cur, indent);
-		cur = cur->next;
-	}
-}
-
-void dumpNode(xmlNodePtr cur, int indent)
-{
-	printIndent(indent);
-	if (cur->type == XML_ELEMENT_NODE && cur->properties)
-	{
-		printf("element \"%s\" (%08X) ns %08X nsdef %08X content %08X\n", cur->name, cur->name, cur->ns, cur->nsDef, cur->content);
-		dumpNodeList((xmlNodePtr) cur->properties, indent + 2);
-		dumpNodeList(cur->children, indent + 2);
-	} else if (cur->type == XML_TEXT_NODE) {
-		printf("text \"%s\" (%08X) value \"%s\" (%08X)\n", cur->name, cur->name, cur->content, cur->content);
-	} else if (cur->type == XML_COMMENT_NODE) {
-		printf("comment \"%s\" (%08X) value \"%s\" (%08X)\n", cur->name, cur->name, cur->content, cur->content);
-	} else if (cur->type == XML_ATTRIBUTE_NODE) {
-		xmlAttrPtr prop = (xmlAttrPtr) cur;
-		printf("attribute %s (%08X) ns %08X atype %d psvi %08X\n", prop->name, prop->name, prop->ns, prop->atype, prop->psvi);
-		dumpNodeList(cur->children, indent + 2);
-	} else {
-		printIndent(indent);
-		printf("!!!!!!!! node type: %d\n", cur->type);
-	}
-}
-#endif
-
-
-/* Необходимо наконец навести порядок с пилением сука, на котором сидит команда.
- * Возможные гадости включают в себя:
- * а) удаление элемента, являющегося предком следующего узла выборки;
- * б) удаление себя/предка;
- * в) удаление узла, входящего в выборку выполняющегося итератора.
- * Выделим две ситуации:
- * 1. Выполнение из итератора (в этом случае узлы не удаляются вообще, а помечаются к удалению
- *    в конце обработки документа через его буфер);
- * 2. Выполнение в общем (противном) случае
- *   2.1. При количестве узлов <= MAX_ELEMENTS_FOR_RELATIONSHIP_SCAN вычисляем треугольник от
- *        декартова произведения узлов. Если узел является предком - удаляем потомков из выборки;
- *   2.2. Иначе используем локальный буфер удаления и осуществляем два прохода (отметка и удаление).
- * Проверка на удаление родителя остаётся и при попытке так сделать маркирует под удаление
- * фрагмент иерархической оси от команды до родителя вне зависимости от выбора в пп. 1-2.
+/* Possible bad things that may happen:
+ * a) deletion of the next node set entry ancestor - with the entry itself;
+ * b) deletion of the command ancestor or the command itself;
+ * c) deletion of a node that is contained in the selection of the currently running iterator.
  * 
- * --ss, 28.12.2013 
+ * Consider three situations:
+ * 1. We're running inside an iterator. We don't really delete any nodes but mark them for deletion instead
+ *    and put them into a buffer which is emptied when document processing is finished;
+ * 2. No iterators are currently running.
+ *    2.1. When we have less than MAX_ELEMENTS_FOR_RELATIONSHIP_SCAN in selection we calculate a triangle
+ *         part of their Cartesian product and remove all descendants of nodes from the selection.
+ *         This is quite expensive - O(M*N^2) ~ O(N^3) and may be removed in future versions;
+ *    2.2. Otherwise we use a local buffer for deletion in a way similar to how global buffer works.
+ *
+ *  These checks can be disabled by setting cfgFoolproofDestructiveCommands to false - however the check
+ *  for AoS deletion is always performed.
  */
 
 #define MAX_ELEMENTS_FOR_RELATIONSHIP_SCAN 8
@@ -167,7 +124,12 @@ void xplCmdDeleteEpilogue(xplCommandInfoPtr commandInfo, xplResultPtr result)
 					xplDocDeferNodeDeletion(commandInfo->document, cur);
 					break;
 				default:
-					/* ToDo: what else?.. we can't touch DTDs anyway */
+					/* we're trying to do something weird like deleting a DTD */
+					if (cfgWarnOnInvalidNodeType)
+						xplDisplayWarning(commandInfo->element, BAD_CAST "attempting to delete a node '%s%s%s', type %d declared at line %d",
+							cur->ns && cur->ns->prefix? cur->ns->prefix: BAD_CAST "",
+							cur->ns && cur->ns->prefix? ":": "",
+							cur->name, cur->type, cur->line);
 					break;
 			} /* switch */
 		} /* for: first pass */
