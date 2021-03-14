8a703a3a1af9a1f7a7c5a45aabbb16cba948b461
dennis <dennis.podkovyrin@gmail.com>
2021-03-14 17:07:22 +0700

    xpl: usage help, version info

--- xpl/xpl.c
+++ xpl/xpl.c
@@ -8,19 +8,14 @@
 #include <libxpl/xploptions.h>
 #include <libxpl/xplsave.h>
 #include <libxpl/xplstart.h>
+#include <libxpl/xplversion.h>
 
-xmlChar* getAppType(void)
+static xmlChar* _getAppType(void)
 {
 	return BAD_CAST "console";
 }
 
-/*
-	-i, --in-file infile.xpl
-	-o, --out-file outfile.xml
-	-s, --start-file configfile.xml
-	-e, --out-encoding output encoding
-	-v, --verbose
-*/
+#define XPR_FEATURES_NEEDED (XPR_STARTSTOP_LOW_LEVEL | XPR_STARTSTOP_CONSOLE)
 
 xmlChar *in_file = BAD_CAST "test.xml";
 xmlChar *out_file = BAD_CAST "test_out.xml";
@@ -46,7 +41,7 @@ static struct option option_list[] =
 		.flag = NULL,
 		.val = 's'
 	}, {
-		.name = "out-out_encoding",
+		.name = "out-encoding",
 		.has_arg = required_argument,
 		.flag = NULL,
 		.val = 'e'
@@ -56,6 +51,16 @@ static struct option option_list[] =
 		.flag = &verbose,
 		.val = 1
 	}, {
+		.name = "help",
+		.has_arg = no_argument,
+		.flag = NULL,
+		.val = 'h'
+	}, {
+		.name = "version",
+		.has_arg = no_argument,
+		.flag = NULL,
+		.val = 'V'
+	}, {
 		.name = NULL,
 		.has_arg = no_argument,
 		.flag = NULL,
@@ -63,14 +68,41 @@ static struct option option_list[] =
 	}
 };
 
+static void _printUsage(void)
+{
+	fprintf(stderr, "XPL batch interpreter.\n");
+	fprintf(stderr, "Usage:\n");
+	fprintf(stderr, "xpl -h/--help\n");
+	fprintf(stderr, "xpl -V/--version\n");
+	fprintf(stderr, "xpl [options]\n");
+	fprintf(stderr, "\t-i/--in-file INFILE (default: test.xpl)\n");
+	fprintf(stderr, "\t-o/--out-file OUTFILE (default: test_out.xml)\n");
+	fprintf(stderr, "\t-e/--out-encoding ENCODING\n");
+	fprintf(stderr, "\t-v/--verbose\n\n");
+}
+
+static void _printVersion(void)
+{
+	xmlChar *libs, *xef;
+
+	fprintf(stderr, "%s\n", XPL_VERSION_FULL);
+	xplInitLibraryVersions();
+	libs = xplLibraryVersionsToString(xplGetCompiledLibraryVersions(), xplGetRunningLibraryVersions());
+	xef = xplXefImplementationsToString(xplGetXefImplementations());
+	fprintf(stderr, "Libraries:\n%sXEF:\n%s", libs, xef);
+	XPL_FREE(libs);
+	XPL_FREE(xef);
+	xplCleanupLibraryVersions();
+}
+
 // TODO windows and file names (wchar_t command line is needed)
-void parseCommandLine(int argc, char **argv)
+static void _parseCommandLine(int argc, char **argv)
 {
 	int option_index;
 
 	while (1)
 	{
-		int c = getopt_long(argc, argv, "i:o:s:e:v", option_list, &option_index);
+		int c = getopt_long(argc, argv, "i:o:s:e:vhV", option_list, &option_index);
 		if (c == -1)
 			break;
 		switch (c)
@@ -92,23 +124,18 @@ void parseCommandLine(int argc, char **argv)
 		case 'v':
 			verbose = true;
 			break;
+		case 'h':
+			_printUsage();
+			exit(EXIT_SUCCESS);
+		case 'V':
+			_printVersion();
+			exit(EXIT_SUCCESS);
 		default:
-			exit(-1);
+			_printUsage();
+			exit(EXIT_FAILURE);
 		}
 	}
-	if (!in_file)
-	{
-		fprintf(stderr, "error: no input file specified\n");
-		exit(-1);
-	}
-	if (!out_file)
-	{
-		fprintf(stderr, "error: no output file specified\n");
-		exit(-1);
-	}
 }
-
-#define XPR_FEATURES_NEEDED (XPR_STARTSTOP_LOW_LEVEL | XPR_STARTSTOP_CONSOLE)
 
 int main(int argc, char* argv[])
 {
@@ -123,7 +150,7 @@ int main(int argc, char* argv[])
 	LEAK_DETECTION_PREPARE
 
 	xplInitMemory(xplDefaultDebugAllocation, xplDefaultUseTcmalloc);
-	parseCommandLine(argc, argv);
+	_parseCommandLine(argc, argv);
 
 	app_path = BAD_CAST getcwd(NULL, 0);
 	memcpy(&start_params, &xplDefaultStartParams, sizeof(xplStartParams));
@@ -141,7 +168,7 @@ int main(int argc, char* argv[])
 
 	if (!cfgDocRoot)
 		cfgDocRoot = BAD_CAST XPL_STRDUP((char*) app_path);
-	xplSetGetAppTypeFunc(getAppType);
+	xplSetGetAppTypeFunc(_getAppType);
 
 	/* create auxiliary structures */
 	session = xplSessionCreateWithAutoId();
